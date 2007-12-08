81da8ce7acd862262679f6fb0da9e10c4e17458b
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-08 12:41:13 +0000

    Ancora fix

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="Rev. 29";
+$game_revision="Rev. 30";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 ?>
