2d86329ee496bc4d0d43862e36f61087a5b06668
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-08 12:14:11 +0000

    Fix in creazione personaggio

--- game/creapersonaggio.php
+++ game/creapersonaggio.php
@@ -9,7 +9,7 @@ if($errore){
 	$outputerrori="<span>Si sono verificati i seguenti errori:</span><br /><span>".$errore."</span><br /><br />";}
 else {
 $db->QueryMod("UPDATE utenti SET personaggio='1' WHERE userid='".$user['userid']."'");
-$db->QueryMod("INSERT INTO caratteristiche (userid,password,codice,email,dataiscrizione,ipreg,server) VALUES ('".$user['userid']."','".$_POST['razza']."')");
+$db->QueryMod("INSERT INTO caratteristiche (userid,password) VALUES ('".$user['userid']."','".$_POST['razza']."')");
 header("Location: game.php?act=situazione");
 exit();
 }
