21a2542b6f90d739a06d5ba8c2326cd5356c51fc
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-08 21:02:58 +0000

    Iniziata creazione menu interno

--- game/inclusi/menu.php
+++ game/inclusi/menu.php
@@ -1,1 +1,19 @@
-menu
+<script type="text/javascript" language="javascript">
+function cambiaclasse(id,newClass) {
+	var identity=document.getElementById(id);
+	identity.className=newClass;
+}
+</script>
+<div id="menu">
+<table width="122"  border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td>
+          <table width="100%"  border="1" cellspacing="1" cellpadding="1" class="tabmenu">		  
+          <tr>
+            <td class="tabmenutd" id="menu1" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=situazione">Situazione</a></div></td>
+          </tr>
+          </table>
+        </td>
+      <tr>
+</table>
+</div>
