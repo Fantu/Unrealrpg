0f713823d6dc5cbaa38641e0d0e45b65101b44ad
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-09 10:00:07 +0000

    Inserito il changelog

---
+++ game/changelog.php
@@ -0,0 +1,10 @@
+<div align="right">
+  <table width="500" border="0" cellspacing="2" cellpadding="2">
+  <tr>
+    <td>Changelog: </td>
+  </tr>
+  <tr>
+    <td><?php include('inclusi/versioni.txt'); ?></td>
+  </tr>
+</table>
+</div>
--- game/inclusi/menu.php
+++ game/inclusi/menu.php
@@ -5,15 +5,12 @@ function cambiaclasse(id,newClass) {
 }
 </script>
 <div id="menu">
-<table width="122"  border="0" align="center" cellpadding="0" cellspacing="0">
-      <tr>
-        <td>
-          <table width="100%"  border="1" cellspacing="1" cellpadding="1" class="tabmenu">		  
-          <tr>
-            <td class="tabmenutd" id="menu1" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=situazione">Situazione</a></div></td>
-          </tr>
-          </table>
-        </td>
-      <tr>
+<table width="122"  border="1" cellspacing="1" cellpadding="1" class="tabmenu">		  
+	<tr>
+    <td class="tabmenutd" id="menu1" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=situazione">Situazione</a></div></td>
+	</tr>
+	<tr>
+    <td class="tabmenutd" id="menu2" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=changelog">Info versione</a></div></td>
+	</tr>	
 </table>
 </div>
