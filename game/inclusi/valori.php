dc9b76f21cbe416194abc4567f7ef7395da95ebf
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-11 17:52:34 +0000

    Migliorati i changelog

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="Rev. 38";
+$game_revision="Rev. 39";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 ?>
