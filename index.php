27612d4933c684158530cd2e4293c2252b76433e
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-11 17:27:21 +0000

    Creata cartella immagini in template e trasferite immagini

--- index.php
+++ index.php
@@ -44,16 +44,16 @@ include('pagine/home.php');
 <?php echo $game_name; ?> &egrave; un prodotto <a href="http://www.lostgames.net" target="_blank">Lostgames.net</a> &copy; 2007.
 <br />
 <a href="http://validator.w3.org/check?uri=referer" target="_blank">
-	<img id="xhtml" src="immagini/xhtml_grigio.gif" alt="" border="0" onmouseover="CambiaImg('xhtml', true);" onmouseout="CambiaImg('xhtml', false);" />
+	<img id="xhtml" src="game/template/immagini/xhtml_grigio.gif" alt="" border="0" onmouseover="CambiaImg('xhtml', true);" onmouseout="CambiaImg('xhtml', false);" />
 </a>&nbsp;
 <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">
-	<img id="css" src="immagini/css_grigio.gif" border="0" alt="" onmouseover="CambiaImg('css', true);" onmouseout="CambiaImg('css', false);" />
+	<img id="css" src="game/template/immagini/css_grigio.gif" border="0" alt="" onmouseover="CambiaImg('css', true);" onmouseout="CambiaImg('css', false);" />
 </a>
 <a href="http://www.php.net" target="_blank">
-	<img id="php" src="immagini/php_grigio.gif" alt="" border="0" onmouseover="CambiaImg('php', true);" onmouseout="CambiaImg('php', false);" />
+	<img id="php" src="game/template/immagini/php_grigio.gif" alt="" border="0" onmouseover="CambiaImg('php', true);" onmouseout="CambiaImg('php', false);" />
 </a>&nbsp;&nbsp;
 <a href="http://www.mysql.com" target="_blank">
-	<img id="mysql" src="immagini/mysql_grigio.gif" alt="" border="0" onmouseover="CambiaImg('mysql', true);" onmouseout="CambiaImg('mysql', false);" />
+	<img id="mysql" src="game/template/immagini/mysql_grigio.gif" alt="" border="0" onmouseover="CambiaImg('mysql', true);" onmouseout="CambiaImg('mysql', false);" />
 </a>&nbsp;&nbsp;<br />
 <?php
 require_once("game/inclusi/errori.php"); 
