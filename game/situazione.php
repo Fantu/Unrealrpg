7c0d667cc41ee72aad094633cf15ccd9e02d94de
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-12 14:08:02 +0000

    Iniziato inserimento dati personaggio in situazione

--- game/situazione.php
+++ game/situazione.php
@@ -1,3 +1,5 @@
 <?php
+$usercar=$db->QuerySelect("SELECT * FROM caratteristiche WHERE userid='".$user['userid']."' LIMIT 0,1");
+require('inclusi/personaggio.php');
 require('template/int_situazione.php');
 ?>
--- game/template/int_situazione.php
+++ game/template/int_situazione.php
@@ -1,1 +1,5 @@
-Situazione<br />
+<center><h2>Situazione</h2></center><br />
+<br />
+Personaggio: <?php echo $user['username']; ?><br />
+Razza: <?php echo $razze['nome'][$user['razza']]; ?><br />
+Classe: <?php echo $classi['nome'][$user['classe']]; ?><br />
