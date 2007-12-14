ea7599100aa62f80a2737b6138e34f764691c398
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-14 12:36:45 +0000

    Prova per fix

--- game/creapersonaggio.php
+++ game/creapersonaggio.php
@@ -11,8 +11,9 @@ $errore .= "- Non hai selezionato la classe<br />";
 if($errore){
 	$outputerrori="<span>Si sono verificati i seguenti errori:</span><br /><span>".$errore."</span><br /><br />";}
 else {
+$agilita=$classi['agilita'][$classe];
 $db->QueryMod("UPDATE utenti SET personaggio='1' WHERE userid='".$user['userid']."'");
-$db->QueryMod("INSERT INTO caratteristiche (userid,razza,classe,agilita,attfisico,attmagico,diffisica,difmagica,mana) VALUES ('".$user['userid']."','".$razza."','".$classe."','".$classi['agilita'][$classe]."','".$classi['attfisico'][$classe]."','".$classi['attmagico'][$classe]."','".$classi['diffisica'][$classe]."','".$classi['difmagica'][$classe]."','".$classi['mana'][$classe]."')");
+$db->QueryMod("INSERT INTO caratteristiche (userid,razza,classe,agilita,attfisico,attmagico,diffisica,difmagica,mana) VALUES ('".$user['userid']."','".$razza."','".$classe."','".$agilita."','".$classi['attfisico'][$classe]."','".$classi['attmagico'][$classe]."','".$classi['diffisica'][$classe]."','".$classi['difmagica'][$classe]."','".$classi['mana'][$classe]."')");
 header("Location: game.php?act=situazione");
 exit();
 }
