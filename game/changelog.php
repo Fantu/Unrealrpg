8e770fc38f75672c6026e48641447a6840a1311e
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-19 13:19:16 +0000

    Piccolo miglioramento

--- game/changelog.php
+++ game/changelog.php
@@ -1,8 +1,11 @@
 <div align="right">
   <table width="500" border="0" cellspacing="2" cellpadding="2">
   <tr>
-    <td>Changelog: </td>
+    <td>Changelog:</td>
   </tr>
+  <tr>
+    <td>Per leggere i changelog completi pi&ugrave; aggiornati dell'intero progetto clicca <a href="game.php?act=changelog&amp;completa=1">qui</a></td>
+  </tr>  
   <tr>
     <td><?php include('inclusi/versioni.txt'); ?></td>
   </tr>
