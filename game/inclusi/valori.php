fec5bfb2cca8efa05321ccac5d1541aa34cc1924
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-19 13:26:05 +0000

    Modificati log per multiversione

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="0.1 R.66";
+$game_revision="0.1 R.67";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 ?>
