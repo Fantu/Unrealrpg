9aad352a9edad9a678f9f11f267f65bc531d4389
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-22 13:36:41 +0000

    Altro fix

--- game/banca.php
+++ game/banca.php
@@ -57,6 +57,7 @@ $prestito=$_POST['inprestito'];
 if (!is_numeric($prestito)){
 $errore .= $lang['banca_errore1'];}
 else{
+$usercar=$db->QuerySelect("SELECT * FROM caratteristiche WHERE userid='".$user['userid']."' LIMIT 0,1");
 $prestitopossibile=($user['livello']*100)-$userbank['prestito'];
 if ($prestito<1)
 $errore .= $lang['banca_errore7'];
