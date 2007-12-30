c9a81c3d42821165bccbe5fbf99f3d620071e7c5
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-30 14:48:42 +0000

    Altro fix

--- crearecord.php
+++ crearecord.php
@@ -5,7 +5,7 @@ require('game/inclusi/funzioni_db.php');
 $db = new ConnessioniMySQL();		
 	foreach($game_server as $chiave=>$elemento){
 	$db->database = $chiave;
-	$a=$db->QuerySelect("SELECT userid FROM utenti WHERE conferma='1' AND personaggio='1'");
+	$a=$db->QueryCiclo("SELECT userid FROM utenti WHERE conferma='1' AND personaggio='1'");
 	while($var=$db->QueryCicloResult($a))
 	{
 		$db->QueryMod("INSERT INTO miniera (userid,ultimolavnuova) VALUES ('".$var['userid']."','".$adesso."')");
