1af2e4e876286abe845b96d854f0b3d3c2aa7523
Fabio Fantoni <fantonifabio@tiscali.it>
2007-12-31 11:56:47 +0000

    Piccolo miglioramento in situazione

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="0.2.1 R.112";
+$game_revision="0.2.1 R.113";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 ?>
