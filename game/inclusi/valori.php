63549a0cdd431356fa925a848d3e48485d12aa35
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-05 14:09:24 +0000

    Prova miglioramento sistema messaggi

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="0.2.5 R.157";
+$game_revision="0.2.5 R.158";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 ?>
--- game/messaggi.php
+++ game/messaggi.php
@@ -8,7 +8,7 @@ function conteggio() {
 </script>
 <div align="center">
 <?php
-if($_GET['do']=="elim") { //cancella mess singolo
+/*if($_GET['do']=="elim") { //cancella mess singolo
 	$db->QueryMod("DELETE FROM messaggi WHERE id='".$_GET['id']."'");
 	echo "<script language=\"javascript\">window.location.href='game.php?act=messaggi'</script>";	
 } else if($_GET['do']=="canc") { //cancella mess selezionati
@@ -56,6 +56,60 @@ if($_GET['do']=="elim") { //cancella mess singolo
 	."</tr></table></form>";
 	$db->QueryMod("UPDATE messaggi SET letto=1 WHERE userid='".$user['userid']."'");
 	}
-} // fine visualizza messaggi
+} // fine visualizza messaggi*/
+
+switch($_GET['do']){
+case "elim":
+$db->QueryMod("DELETE FROM messaggi WHERE id='".$_GET['id']."'");
+echo "<script language=\"javascript\">window.location.href='game.php?act=messaggi'</script>";
+break;
+case "canc":
+while($_POST['contatore']>0) {	
+	$db->QueryMod("DELETE FROM messaggi WHERE id='".$_POST['messaggioid'.$_POST['contatore'].'']."'");
+	$_POST['contatore']--;
+}
+echo "<script language=\"javascript\">window.location.href='game.php?act=messaggi'</script>";
+break;
+default:	
+$a=$db->QueryCiclo("SELECT * FROM messaggi WHERE userid='".$user['userid']."' ORDER BY id desc");
+	$esistenza=mysql_num_rows($a);
+	if (!$esistenza){
+		echo "Non c'&egrave; nessun messaggio";}
+	else{
+	echo "<form action=\"game.php?act=messaggi&amp;do=canc\" method=\"post\" name=\"canctutt\">";
+	$i=0;
+	while($mess=$db->QueryCicloResult($a)) {
+		$i++;
+		if ($mess['mittenteid']!=0)
+		$mit=$db->QuerySelect("SELECT username FROM utenti WHERE userid='".$mess['mittenteid']."'");
+
+	?>
+	<table width="505"  border="0" cellspacing="2" cellpadding="2">
+	  <tr>
+		<td width="5%"><?php echo "<input name=\"messaggioid".$i."\" type=\"checkbox\" id=\"messaggioid".$i."\" value=\"".$mess['id']."\" />"; ?></td>
+		<td width="95%"><div align="center">Messaggio da: <?php if ($mess['mittenteid']==0){echo "Sistema";}else{echo $mit['username'];} ?> - in data: <?php echo date("d/m/y - H:i",$mess['data']); ?> </div></td>
+	  </tr>
+	  <tr>
+		<td>&nbsp;</td>
+		<td><?php echo "<strong><span>".$mess['titolo']."</span></strong><br />".$mess['testo']; ?></td>
+	  </tr>
+	  <tr>
+		<td colspan="2" align="right"> 
+		<?php 
+		if($mess['mittenteid']==0)
+			echo "[ <a href=\"game.php?act=messaggi&amp;do=elim&amp;id=".$mess['id']."\">Elimina</a> ]";
+		else
+			echo "[ <a href=\"game.php?act=messaggi&amp;do=risp&amp;id=".$mess['id']."\">Rispondi</a> ] - [ <a href=\"game.php?act=messaggi&amp;do=elim&amp;id=".$mess['id']."\">Elimina</a> ]"; ?></td>
+	  </tr>
+	</table>
+	<?php
+	}
+	echo "<br /><table width=\"505\"  border=\"0\" cellspacing=\"2\" cellpadding=\"2\"><tr>"
+    ."<td align=\"center\"><input name=\"contatore\" type=\"hidden\" value=\"".$i."\" /><input name=\"asd\" type=\"submit\" value=\"Cancella i selezionati\" /></td>"
+	."</tr></table></form>";
+	$db->QueryMod("UPDATE messaggi SET letto=1 WHERE userid='".$user['userid']."'");
+	}
+break;
+}
 ?>
 </div>
