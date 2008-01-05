0b11e48af1cd887cc15ca61b23b93e761609343e
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-05 11:30:56 +0000

    Creata ricerca personaggio

--- game/template/int_utenti.php
+++ game/template/int_utenti.php
@@ -1,6 +1,46 @@
 <center><h2><?php echo $lang['Lista_utenti']; ?></h2></center><br />
 <br />
-In sviluppo...<br />
+<?php echo $outputerrori; ?>
+<?php if($ricerca==1){/*inizio mostra risultati ricerca*/ ?>
+<br />
+<br />
+<table width="500" border="1" cellspacing="2" cellpadding="2">
+  <tr>
+    <td><div align="center"><?php echo $lang['Personaggio']; ?></div></td>
+    <td><div align="center"><?php echo $lang['Stato']; ?></div></td>
+  </tr>
+<?php foreach($utenti['nome'] as $chiave=>$elemento){ ?>
+<tr>
+<td><div align="center"><?php echo $utenti['nome'][$chiave]; ?></div></td>
+<td><div align="center"><?php if($utenti['online'][$chiave]==1){ ?>
+<img src="template/immagini/led_verde.gif" alt="Online" />
+<?php }else{ ?>
+<img src="template/immagini/led_rosso.gif" alt="Offline" />
+<?php } ?>
+</div></td>
+</tr>
+<?php }/* fine per ogni utente della lista*/ ?>
+</table>
+<?php }/*fine mostra risultati ricerca*/ ?>
+<br />
+<br />
+<div align="center">
+<form action="game.php?act=utenti?azione=cerca" method="post" name="formcu">
+<table width="300" border="0" cellspacing="2" cellpadding="2" align="center">
+  <tr>
+    <td colspan="2"><div align="center">Cerca utente per nome</div></td>
+    </tr>
+  <tr>
+    <td width="50%">
+      <div align="right">
+        <input name="nome" type="text" id="nome" maxlength="20" />
+      </div></td>
+    <td width="50%"><input type="submit" name="cercau" value="Cerca" /></td>
+  </tr>
+</table>
+</form>
+</div>
+<br />
 <br />
 <table width="500" border="1" cellspacing="2" cellpadding="2">
   <tr>
--- game/utenti.php
+++ game/utenti.php
@@ -1,4 +1,29 @@
 <?php
+if (isset($_POST['crea'])){
+$errore="";
+if(!$_POST['nome'])
+	$errore="Non hai scritto il nome da cercare";
+else {
+	$utentecercato=$db->QueryCiclo("SELECT userid,username,ultimazione FROM utenti WHERE LIKE '%'".$_POST['nome']."'%' AND conferma='1' AND personaggio='1'");
+	if(!$utentecercato['userid'])
+		$errore="Non esiste nessun personaggio con il nome che contiene (".$_POST['nome'].").";
+}
+if($errore){
+$outputerrori="<span>".$lang['outputerrori']."</span><br /><span>".$errore."</span><br /><br />";
+}else{//inizio mostra risultati
+$ricerca=1;
+//$a=$db->QueryCiclo("SELECT userid,username,ultimazione FROM utenti WHERE LIKE '%'".$_POST['nome']."'%' AND conferma='1' AND personaggio='1'");
+$i=0;
+$seonline=$adesso-600;
+while($chi=$db->QueryCicloResult($utentecercato)) {
+	$i++;
+	$utenti['nome'][$i]=$chi['username'];
+	if ($chi['ultimazione']>$seonline){
+	$utenti['online'][$i]=1;}else
+	{$utenti['online'][$i]=0;}
+}
+}//fine mostra risultati
+}else{//inizio se non cerca
 $ordine="ORDER BY userid ASC";
 switch($_GET['ordine']){
 case "stato":
@@ -15,7 +40,7 @@ $ordine="ORDER BY username ASC";
 }else{
 $ordine="ORDER BY userid ASC";
 }//fine ordinamenti*/
-$a=$db->QueryCiclo("SELECT userid,username,ultimazione FROM utenti WHERE conferma='1' AND personaggio='1''".$ordine."'");
+$a=$db->QueryCiclo("SELECT userid,username,ultimazione FROM utenti WHERE conferma='1' AND personaggio='1' '".$ordine."'");
 $i=0;
 $seonline=$adesso-600;
 while($chi=$db->QueryCicloResult($a)) {
@@ -25,5 +50,6 @@ while($chi=$db->QueryCicloResult($a)) {
 	$utenti['online'][$i]=1;}else
 	{$utenti['online'][$i]=0;}
 }
+}//fine se non cerca
 require('template/int_utenti.php');
 ?>
