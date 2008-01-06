af789d762c46a82f41562a64dc935458e1cc83d9
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-06 16:39:22 +0000

    Altra prova

--- game/visualizzautente.php
+++ game/visualizzautente.php
@@ -2,6 +2,7 @@
 require('inclusi/personaggio.php');
 //$utente=$_POST['visualizzaid'];
 //$datiutente=$db->QuerySelect("SELECT t1.userid,t1.username AS username,t1.ultimazione AS ultimazione,t2.livello AS livello,t2.razza AS razza,t2.classe AS classe FROM utenti AS t1 JOIN caratteristiche t2 ON t1.userid=t2.userid WHERE userid='5' LIMIT 0,1");
+$db->database = $lg[3];
 $datiutente=$db->QuerySelect("SELECT * FROM utenti WHERE userid='5'");
 require('template/int_visualizzautente.php');
 ?>
