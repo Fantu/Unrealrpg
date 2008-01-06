0a8337894e94fbc7410a8aa36d1e373ce8c10015
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-06 17:25:03 +0000

    Rilasciata BETA 0.2.6

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="0.2.6 R.171";
+$game_revision="0.2.6 R.190";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 ?>
