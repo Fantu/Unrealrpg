b62f1597a0271ec5f5c35228444d82e0ead05fc5
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-09 19:58:24 +0000

    Creazione base lavoro apprendista in laboratorio, abilità alchimista ecc Aggiunta creazione record per tabella laboratorio

--- game/laboratorio.php
+++ game/laboratorio.php
@@ -1,4 +1,33 @@
 <?php
 require('language/it/lang_laboratorio.php');
+if (isset($_POST['lavoralabapp'])){
+$errore="";
+$usercar=$db->QuerySelect("SELECT * FROM caratteristiche WHERE userid='".$user['userid']."' LIMIT 0,1");
+$userlab=$db->QuerySelect("SELECT * FROM laboratorio WHERE userid='".$user['userid']."' LIMIT 0,1");
+if ($usercar['energia']<100)
+$errore .= $lang['lab_errore1'];
+if ($usercar['saluteattuale']<30)
+$errore .= $lang['lab_errore2'];
+if ($adesso<($usermin['ultimolavapp']+21600))
+$errore .= $lang['lab_errore3'];
+if($errore){
+	$outputerrori="<span>".$lang['outputerrori']."</span><br /><span>".$errore."</span><br /><br />";}
+else {
+$paga=5;
+$energia=100-(5*$usercar['alchimista']);
+if ($energia<50)
+$energia=50;
+$salute=(rand(2,10))-(1*$usercar['alchimista'])-(rand(0,floor($usercar['difmagica']/10)));
+if ($salute<1)
+$salute=1;
+$exp=rand(5,(2+floor($usercar['saluteattuale']/10)+floor($usercar['energia']/100)+floor($usercar['attmagico']/10)));
+$exp+=(5*(rand(0,$usercar['alchimista'])));
+$db->QueryMod("UPDATE laboratorio t1 JOIN utenti t2 on t1.userid=t2.userid JOIN caratteristiche t3 on t2.userid=t3.userid SET t1.ultimolavapp='".$adesso."',t3.expalchimista=t3.expalchimista+'".$exp."',t2.monete=t2.monete+'".$paga."',t3.energia=t3.energia-'".$energia."',t3.saluteattuale=t3.saluteattuale-'".$salute."' WHERE t1.userid='".$user['userid']."'");
+$testo="<span>".sprintf($lang['report_lavlabapp'],$paga,$exp,$energia,$salute)."</span><br /><br />";
+$titolo=$lang['report_lavoro_labapp'];
+$db->QueryMod("INSERT INTO messaggi (userid,titolo,testo,mittenteid,data) VALUES ('".$user['userid']."','".$titolo."','".$testo."','0','".$adesso."')");
+}
+}//fine lavora come apprendista
+
 require('template/int_laboratorio.php');
 ?>
--- game/language/it/lang_laboratorio.php
+++ game/language/it/lang_laboratorio.php
@@ -1,9 +1,9 @@
 <?php
 $lang['lav_apprendista']='Lavora come apprendista';
 $lang['desc_lav_apprendista']='Puoi lavorare in laboratorio come apprendista avendo una retribuzione fissa di 5 monete ad ora e guadagnare esperienza per l\'abilit&agrave; di alchimista utile per avere risultati migliori con meno fatica e meno danni';
-$lang['miniera_errore1']='- Sei troppo stanco per poter lavorare in miniera<br />';
-$lang['miniera_errore2']='- Il tuo fisico non pu&ograve; reggere il lavoro in miniera attualmente<br />';
-$lang['miniera_errore3']='- Devono passare almeno 6 ore dall\'ultimo lavoro in miniera<br />';
-$lang['report_lavminieranuova']='Hai lavorato in miniera nuova per 1 ora guadagnando %s monete e %s di esperienza come minatore, il duro lavoro ti ha stancato (hai perso %s di energia) e il tuo fisico ne ha risentito (hai perso %s di salute)';
-$lang['report_lavoro_nuova']='Report lavoro in miniera nuova';
+$lang['lab_errore1']='- Sei troppo stanco per poter lavorare in miniera<br />';
+$lang['lab_errore2']='- Il tuo fisico non pu&ograve; reggere il lavoro in miniera attualmente<br />';
+$lang['lab_errore3']='- Devono passare almeno 6 ore dall\'ultimo lavoro in laboratorio<br />';
+$lang['report_lavlabapp']='Hai lavorato in laboratorio come apprendista per 1 ora guadagnando %s monete e %s di esperienza, il lavoro ti ha stancato (hai perso %s di energia) e il tuo fisico ne ha risentito (hai perso %s di salute)';
+$lang['report_lavoro_labapp']='Report lavoro in laboratorio come apprendista';
 ?>
