7ef5e889448e7862cf6559548463153619db243c
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-10 18:16:19 +0000

    Aggiunta la possibilità di incidenti in laboratorio (esplosione delle pozioni che si stanno creando) Forza magica e livello di alchimia influiscono sulla possibilit&grave; che accadano incidenti in laboratorio Agilità aumenta la probilità di schivare le esplosioni in laboratorio Forza magica aumenta la probilità di schivare le esplosioni in laboratorio Resistenza magica diminuisce il danno subito nelle esplosioni in laboratorio

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="0.2.11 R.209";
+$game_revision="0.2.11 R.210";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 ?>
--- game/language/it/lang_laboratorio.php
+++ game/language/it/lang_laboratorio.php
@@ -6,4 +6,7 @@ $lang['lab_errore2']='- Il tuo fisico non pu&ograve; reggere il lavoro in minier
 $lang['lab_errore3']='- Devono passare almeno 6 ore dall\'ultimo lavoro in laboratorio<br />';
 $lang['report_lavlabapp']='Hai lavorato in laboratorio come apprendista per 1 ora guadagnando %s monete e %s di esperienza, il lavoro ti ha stancato (hai perso %s di energia) e il tuo fisico ne ha risentito (hai perso %s di salute)';
 $lang['report_lavoro_labapp']='Report lavoro in laboratorio come apprendista';
+$lang['report_esplosione_laboratorio']='Imprevisto durante il lavoro in laboratorio';
+$lang['report_esplosione_lab1']='Durante il tuo lavoro in laboratorio &egrave; esplosa una pozione ma sei riuscito ad allontanarti in tempo e non ferirti';
+$lang['report_esplosione_lab2']='Durante il tuo lavoro in laboratorio &egrave; esplosa una pozione, non sei riuscito ad allontanarti in tempo e ti sei ferito (%s di salute persa)';
 ?>
