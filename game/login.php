64eec6c06a4a903c748c3e17f474cf6e4a1e2612
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-12 23:39:01 +0000

    Migliorato sistema login

--- game/login.php
+++ game/login.php
@@ -1,23 +1,32 @@
 <?php
 require('inclusi/funzioni_db.php');
+require('inclusi/valori.php');
 $db = new ConnessioniMySQL();
-$db->database=$_POST['login_server'];
-
+$esistenza=0;		
+	foreach($game_server as $chiave=>$elemento){
+	if ($chiave==$_POST['login_server']){$esistenza=1;}
+	}
+if($esistenza==0){
+	header("Location: ../index.php?error=3");
+	exit();
+} else{
+$db->database=$_POST['login_server'];	
 $_POST['login_username']=str_replace("'","\'",$_POST['login_username']);
 $user = $db->QuerySelect("SELECT * FROM utenti WHERE username='".$_POST['login_username']."' AND password='".md5($_POST['login_password'])."' LIMIT 0,1");
-
-$check = $db->QuerySelect("SELECT chiuso FROM config");
-
+}
 if(!$user['userid']) {
 	header("Location: ../index.php?error=1");
 	exit();
-} else if($user['conferma']==0) {
+}
+if($user['conferma']==0) {
 	header("Location: ../index.php?error=2");
 	exit();
 /*} else if($user['bloccato']>0 && strtotime("now")<$user['bloccato']) {
 	header("Location: ../index.php?error=11&t=".$user['bloccato']);	
 	exit();*/
-} else if($check['chiuso']==1) {
+}
+$check = $db->QuerySelect("SELECT chiuso FROM config");
+if($check['chiuso']==1) {
 	header("Location: ../index.php?error=12");
 	exit();
 } else {      
