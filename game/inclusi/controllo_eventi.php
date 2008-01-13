7e786d4c95cddd31da352d816ff0c23add67a6ae
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-13 13:49:10 +0000

    Altro miglioramento

--- game/inclusi/controllo_eventi.php
+++ game/inclusi/controllo_eventi.php
@@ -2,7 +2,8 @@
 require_once('inclusi/funzioni_eventi.php');
 //controllo ultimo evento
 //$evfiniti=$db->QueryCiclo("SELECT * FROM eventi WHERE userid='".$user['userid']."' LIMIT 1");
-$evfiniti=$db->QueryCiclo("SELECT * FROM eventi WHERE (('datainizio'+'secondi')<'".$adesso."') LIMIT 1");
+//$evfiniti=$db->QueryCiclo("SELECT * FROM eventi WHERE (('datainizio'+'secondi')<'".$adesso."') LIMIT 1");
+$evfiniti=$db->QueryCiclo("SELECT * FROM eventi ORDER BY datainizio ASC LIMIT 2");
 while($evento=$db->QueryCicloResult($evfiniti)) {
 if( ($evento['datainizio']+$evento['secondi'])<$adesso ) {
 		switch($evento['tipo']){
