414ee4d399eef7cb9f3aa4e64e2e7dee00568e6e
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-25 18:17:34 +0000

    Prova fix

--- game/mercato.php
+++ game/mercato.php
@@ -13,7 +13,7 @@ $categoria=(int)$_GET['categoria'];
 if(is_array($catoggetti_nome[$categoria])){
 foreach($catoggetti_nome[$categoria] as $chiave=>$elemento){
 $i++;
-$catoggetti[$i]="<a href=\"game.php?act=mercato&amp;step=2&amp;categoria=".$chiave."\">".$lang['categoria'.$categoria.'-'.$chiave]."</a>";
+$catoggetti[$i]="<a href=\"game.php?act=mercato&amp;step=2&amp;categoria=".$chiave."\">".$lang['categoria'.$categoria.'-'.$elemento]."</a>";
 }
 }//fine se ci sono sottocategorie
 else
