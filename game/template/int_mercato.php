da2d23e99efc8c803062079e667db11d11527dee
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-25 19:31:49 +0000

    Altra prova fix

--- game/template/int_mercato.php
+++ game/template/int_mercato.php
@@ -3,15 +3,15 @@
 if($mostraogg==1){
 	if ($seoggetti['id']==0){
 	echo $nessunogg."<br />";
-	}else{?>
+	}else{ ?>
 	<table border="0">
 	<tr><td><?php echo $lang['Nome']; ?></td></tr><tr><td><?php echo $lang['Costo']; ?></td></tr>
 	<?php foreach($oggetti['nome'] as $chiave=>$elemento){?>
 	<tr><td><?php echo $oggetti['nome'][$chiave]; ?></td></tr><tr><td><?php echo $oggetti['costo'][$chiave]; ?></td></tr>
 	<?php }/*fine ogni oggetto*/ ?>
 	</table>
-	<?php}//fine mostra oggetti
+	<?php }//fine mostra oggetti
 }else{
 foreach($catoggetti as $chiave=>$elemento){
-echo $catoggetti[$chiave]."<br />";}	
+echo $catoggetti[$chiave]."<br />";}
 }?>
