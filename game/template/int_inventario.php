ffc9210e026f8a7f0ac3122f0e7e0615d743547f
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-25 10:46:05 +0000

    Fix ultima revisione

--- game/template/int_inventario.php
+++ game/template/int_inventario.php
@@ -1,4 +1,5 @@
 <center><h2><?php echo $lang['Inventario']; ?></h2></center><br />
 <?php if(!empty($nessunogg)){ echo $nessunogg."<br />";}else{
 foreach($oggetti['nome'] as $chiave=>$elemento){
-echo $oggetti['numero'][$chiave]." ".$oggetti['nome'][$chiave] ?>
+echo $oggetti['numero'][$chiave]." ".$oggetti['nome'][$chiave]."<br />";
+} ?>
