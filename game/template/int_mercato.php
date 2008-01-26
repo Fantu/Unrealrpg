114cc63dd6c5e5f1550f3853e67c5a8069469b7a
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-26 09:32:48 +0000

    Altro miglioramento

--- game/template/int_mercato.php
+++ game/template/int_mercato.php
@@ -6,13 +6,13 @@ if($mostraogg==1){
 	echo $nessunogg."<br />";
 	}else{ ?>
 	<form action="" method="post" name="formcompra">
-	<table border="0">
+	<table border="0" align="center" border="1">
 	<tr><td><?php echo $lang['Nome']; ?></td><td><?php echo $lang['Costo']; ?></td></tr>
 	<?php foreach($oggetti['nome'] as $chiave=>$elemento){?>
 	<tr><td><input type="radio" name="oggselect" value="<?php echo $oggetti['id'][$chiave]; ?>" /><?php echo $oggetti['nome'][$chiave]; ?></td><td><?php echo $oggetti['costo'][$chiave]; ?></td></tr>
 	<?php }/*fine ogni oggetto*/ ?>
 	</table>
-	<?php echo $lang['Quanti']; ?> <input name="quanti" type="text" maxlength="2" size="4" />
+	<?php echo $lang['Quanti']; ?> <input name="quanti" type="text" maxlength="2" size="3" />
 	<input type="submit" name="compra" value="<?php echo $lang['Compra']; ?>" />
 	</form>
 	<?php }//fine mostra oggetti
