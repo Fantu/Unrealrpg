9c5b1fb882150464144bf7b5a463e9d999b1a11a
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-26 16:51:00 +0000

    Aggiunti 9 minerali

--- game/language/it/lang_oggetti_nomi.php
+++ game/language/it/lang_oggetti_nomi.php
@@ -1,3 +1,12 @@
 <?php
 $lang['oggetto1_nome']='Torcia';
+$lang['oggetto2_nome']='Carbone';
+$lang['oggetto3_nome']='Rame';
+$lang['oggetto4_nome']='Ferro';
+$lang['oggetto5_nome']='Argento';
+$lang['oggetto6_nome']='Oro';
+$lang['oggetto7_nome']='Rubini';
+$lang['oggetto8_nome']='Smeraldi';
+$lang['oggetto9_nome']='Diamanti';
+$lang['oggetto10_nome']='Mithrill';
 ?>
