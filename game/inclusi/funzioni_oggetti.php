ca9bd9dccd3653031b8e1887fa15879a7a377326
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-27 20:08:50 +0000

    fix

--- game/inclusi/funzioni_oggetti.php
+++ game/inclusi/funzioni_oggetti.php
@@ -16,13 +16,13 @@ $rotto=0;
 $usura=$ogg['usura']+1;
 if($usura==$oggetto['usura']){
 $rotto=1;
-$oggpersi.=sprintf($lang['oggetto_usurato'],$lang['oggetto'.$ogg['oggid'].'_nome'])."<br />;
+$oggpersi.=sprintf($lang['oggetto_usurato'],$lang['oggetto'.$ogg['oggid'].'_nome'])."<br />";
 }else{
 $rottura=floor($oggetto['probrottura']/$oggetto['usura']*$usura);
 $prob=rand(1,10000);
 if($prob<$rottura){
 $rotto=1;
-$oggpersi.=sprintf($lang['oggetto_rotto'],$lang['oggetto'.$ogg['oggid'].'_nome'])."<br />;
+$oggpersi.=sprintf($lang['oggetto_rotto'],$lang['oggetto'.$ogg['oggid'].'_nome'])."<br />";
 }
 }
 if($rotto==1){
