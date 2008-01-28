398a890fbca117cd1e56721913dcdc11aa15baf5
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-28 15:18:17 +0000

    Pulizia codice

--- game/template/int_menu.php
+++ game/template/int_menu.php
@@ -1,52 +1,46 @@
-<script type="text/javascript" language="javascript">
-function cambiaclasse(id,newClass) {
-	var identity=document.getElementById(id);
-	identity.className=newClass;
-}
-</script>
 <div id="menu">
 <table width="122"  border="1" cellspacing="1" cellpadding="1" class="tabmenu">		  
 	<tr>
-    <td class="tabmenutd" id="menu1" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=situazione"><?php echo $lang['Situazione']; ?></a></div></td>
+    <td id="menu1"><div align="center"><a href="game.php?act=situazione"><?php echo $lang['Situazione']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu2" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=banca"><?php echo $lang['Banca']; ?></a></div></td>
+    <td id="menu2"><div align="center"><a href="game.php?act=banca"><?php echo $lang['Banca']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu3" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=miniera"><?php echo $lang['Miniera']; ?></a></div></td>
+    <td id="menu3"><div align="center"><a href="game.php?act=miniera"><?php echo $lang['Miniera']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu4" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=laboratorio"><?php echo $lang['Laboratorio']; ?></a></div></td>
+    <td id="menu4"><div align="center"><a href="game.php?act=laboratorio"><?php echo $lang['Laboratorio']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu5" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=tempio"><?php echo $lang['Tempio']; ?></a></div></td>
+    <td id="menu5"><div align="center"><a href="game.php?act=tempio"><?php echo $lang['Tempio']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu6" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=mercato"><?php echo $lang['Mercato']; ?></a></div></td>
+    <td id="menu6"><div align="center"><a href="game.php?act=mercato"><?php echo $lang['Mercato']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu7" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=inventario"><?php echo $lang['Inventario']; ?></a></div></td>
+    <td id="menu7"><div align="center"><a href="game.php?act=inventario"><?php echo $lang['Inventario']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu8" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=messaggi"><?php echo $lang['Messaggi']; ?></a></div></td>
+    <td id="menu8"><div align="center"><a href="game.php?act=messaggi"><?php echo $lang['Messaggi']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu9" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=utenti"><?php echo $lang['Lista_utenti']; ?></a></div></td>
+    <td id="menu9"><div align="center"><a href="game.php?act=utenti"><?php echo $lang['Lista_utenti']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu10" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=guida"><?php echo $lang['Guida']; ?></a></div></td>
+    <td id="menu10"><div align="center"><a href="game.php?act=guida"><?php echo $lang['Guida']; ?></a></div></td>
 	</tr>				
 	<tr>
-    <td class="tabmenutd" id="menu11" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=changelog"><?php echo $lang['Changelog']; ?></a></div></td>
+    <td id="menu11"><div align="center"><a href="game.php?act=changelog"><?php echo $lang['Changelog']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu12" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=opzioni"><?php echo $lang['Opzioni']; ?></a></div></td>
+    <td id="menu12"><div align="center"><a href="game.php?act=opzioni"><?php echo $lang['Opzioni']; ?></a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu13" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="http://www.lostgames.net/forum/forumdisplay.php?f=33" target="_blank">Forum</a></div></td>
+    <td id="menu13"><div align="center"><a href="http://www.lostgames.net/forum/forumdisplay.php?f=33" target="_blank">Forum</a></div></td>
 	</tr>
 	<tr>
-    <td class="tabmenutd" id="menu14" onmouseover="cambiaclasse(this.id,'tabmenutd2')" onmouseout="cambiaclasse(this.id,'tabmenutd')"><div align="center"><a href="game.php?act=logout"><?php echo $lang['Logout']; ?></a></div></td>
+    <td id="menu14"><div align="center"><a href="game.php?act=logout"><?php echo $lang['Logout']; ?></a></div></td>
 	</tr>		
 </table>
 </div>
