48b674df21b778403aedea837a334a7edb1decd5
Fabio Fantoni <fantonifabio@tiscali.it>
2008-01-28 11:04:48 +0000

    Tempo di resurrezione ridotto di 1 ora per gli Account Plus

--- game/inclusi/morte.php
+++ game/inclusi/morte.php
@@ -5,11 +5,12 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 }
 if ($user['resuscita']=='0'){
 $tempor=86400-$usercar['fede'];
-if ($tempor<3600)
-$tempor=3600;
 }else{
 $tempor=36000;	
 }
+if($user['plus']!=0){$tempor-=3600}
+if ($tempor<3600)
+$tempor=3600;
 $db->QueryMod("INSERT INTO eventi (userid,datainizio,secondi,dettagli,tipo) VALUES ('".$user['userid']."','".$adesso."','".$tempor."','4','3')");
 header("Location: game.php?act=situazione");
 exit();
