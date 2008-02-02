60ee387911d5a2df425449205da7f65c5beb914c
Fabio Fantoni <fantonifabio@tiscali.it>
2008-02-02 16:41:51 +0000

    vari miliogramenti

--- game/guida.php
+++ game/guida.php
@@ -1,4 +1,8 @@
 <?php
+if((empty($int_security)) OR ($int_security!=$game_se_code)){
+	header("Location: ../index.php?error=16");
+	exit();
+}
 require('inclusi/personaggio.php');
 require('language/it/lang_guida.php');
 require('template/int_guida.php');
--- game/template/int_inventario.php
+++ game/template/int_inventario.php
@@ -1,3 +1,9 @@
+<?php
+if((empty($int_security)) OR ($int_security!=$game_se_code)){
+	header("Location: ../index.php?error=16");
+	exit();
+}
+?>
 <center><h2><?php echo $lang['Inventario']; ?></h2></center><br />
 <br />
 <?php echo $outputerrori; ?>
