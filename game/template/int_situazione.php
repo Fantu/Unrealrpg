0bc657443783b6b61104bedae2fa1176c99bc977
Fabio Fantoni <fantonifabio@tiscali.it>
2008-02-11 22:37:20 +0000

    miglioramento

--- game/template/int_situazione.php
+++ game/template/int_situazione.php
@@ -38,14 +38,18 @@ echo "<span><strong>".$lang['comunicazione']."</strong>".$newscom['comunicazione
 <?php echo $lang['Monete']; ?>: <?php echo $user['monete']; ?><br />
 <br />
 <table align="center" border="1" cellspacing="1">
-<tr><td><?php echo $lang['ab_minatore']; ?>: <?php echo $usercar['minatore']; ?> </td>
-<td width="<?php echo $percmin1; ?>" class="sfondoverde"></td><td width="<?php echo $percmin2; ?>" class="sfondorosso"></td></tr>
-<tr><td><?php echo $lang['ab_alchimista']; ?>: <?php echo $usercar['alchimista']; ?> </td>
-<td width="<?php echo $percmin3; ?>" class="sfondoverde"></td><td width="<?php echo $percmin4; ?>" class="sfondorosso"></td></tr>
-<tr><td><?php echo $lang['ab_fabbro']; ?>: <?php echo $usercar['fabbro']; ?> </td>
-<td width="<?php echo $percmin5; ?>" class="sfondoverde"></td><td width="<?php echo $percmin6; ?>" class="sfondorosso"></td></tr>
-<tr><td><?php echo $lang['ab_magica']; ?>: <?php echo $usercar['magica']; ?> </td>
-<td width="<?php echo $percmin7; ?>" class="sfondoverde"></td><td width="<?php echo $percmin8; ?>" class="sfondorosso"></td></tr>
+<tr><td><?php echo $lang['ab_minatore']; ?>: <?php echo $usercar['minatore']; ?></td>
+<td><table align="center" border="0" cellspacing="0"><tr><td width="<?php echo $percmin1; ?>" class="sfondoverde"></td><td width="<?php echo $percmin2; ?>" class="sfondorosso"></td></tr>
+</table></tr>
+<tr><td><?php echo $lang['ab_alchimista']; ?>: <?php echo $usercar['alchimista']; ?></td>
+<td><table align="center" border="0" cellspacing="0"><tr><td width="<?php echo $percmin3; ?>" class="sfondoverde"></td><td width="<?php echo $percmin4; ?>" class="sfondorosso"></td></tr>
+</table></tr>
+<tr><td><?php echo $lang['ab_fabbro']; ?>: <?php echo $usercar['fabbro']; ?></td>
+<td><table align="center" border="0" cellspacing="0"><tr><td width="<?php echo $percmin5; ?>" class="sfondoverde"></td><td width="<?php echo $percmin6; ?>" class="sfondorosso"></td></tr>
+</table></tr>
+<tr><td><?php echo $lang['ab_magica']; ?>: <?php echo $usercar['magica']; ?></td>
+<td><table align="center" border="0" cellspacing="0"><tr><td width="<?php echo $percmin7; ?>" class="sfondoverde"></td><td width="<?php echo $percmin8; ?>" class="sfondorosso"></td></tr>
+</table></tr>
 </table>
 <br />
 <?php echo $proxlav; ?><br />
