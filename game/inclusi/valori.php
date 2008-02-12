2dfe4895ecd504865a7aa72d2547af57860f0904
Fabio Fantoni <fantonifabio@tiscali.it>
2008-02-12 22:30:46 +0000

    rimodificato log

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="0.5.1 R.565";
+$game_revision="0.5.1 R.567";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 $game_se_code="f87d5gf945fhut";
