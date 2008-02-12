c009302feef592830a830e19614cdf85cb207794
Fabio Fantoni <fantonifabio@tiscali.it>
2008-02-12 12:19:45 +0000

    fix

--- game/language/it/lang_fucina.php
+++ game/language/it/lang_fucina.php
@@ -5,9 +5,9 @@ $lang['lavora_fucina']='Lavora in Fucina';
 $lang['fucina_errore1']='- Sei troppo stanco per poter lavorare in Fucina<br />';
 $lang['fucina_errore2']='- Il tuo fisico non pu&ograve; reggere il lavoro in Fucina attualmente<br />';
 $lang['fucina_errore3']='- Devono passare almeno 6 ore dall\'ultimo lavoro<br />';
-$lang['report_lav_fuc_app']='Hai lavorato in Fucina come apprendista per 1 ora guadagnando %s monete e %s di esperienza come minatore, il duro lavoro ti ha stancato (hai perso %s di energia) e il tuo fisico ne ha risentito (hai perso %s di salute)';
+$lang['report_lav_fuc_app']='Hai lavorato in Fucina come apprendista per 1 ora guadagnando %s monete e %s di esperienza come fabbro, il duro lavoro ti ha stancato (hai perso %s di energia) e il tuo fisico ne ha risentito (hai perso %s di salute)';
 $lang['report_lavoro_fucina_app']='Completato lavoro in Fucina come apprendista';
 $lang['report_incidente_fucina']='Imprevisto durante il lavoro in Fucina';
-$lang['report_incidente_fuc1']='Durante il tuo lavoro in miniera si &egrave; verificato un incidente ma sei riuscito ad allontanarti in tempo e non ferirti';
-$lang['report_incidente_fuc2']='Durante il tuo lavoro in miniera si &egrave; verificato un incidente, non sei riuscito ad allontanarti in tempo e ti sei ferito (%s di salute persa)';
+$lang['report_incidente_fuc1']='Durante il tuo lavoro in Fucina si &egrave; verificato un incidente ma sei riuscito ad allontanarti in tempo e non ferirti';
+$lang['report_incidente_fuc2']='Durante il tuo lavoro in Fucina si &egrave; verificato un incidente, non sei riuscito ad allontanarti in tempo e ti sei ferito (%s di salute persa)';
 ?>
