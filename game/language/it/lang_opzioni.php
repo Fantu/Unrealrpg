66440cb3933aa7577b5b0834416be92ff535418b
Fabio Fantoni <fantonifabio@tiscali.it>
2008-02-13 22:53:07 +0000

    miglioramenti

--- game/language/it/lang_opzioni.php
+++ game/language/it/lang_opzioni.php
@@ -7,9 +7,9 @@ $lang['plus_non_attivo']='Account plus non attivo';
 $lang['quanti_punti_plus']='Hai %s punti plus';
 $lang['desc_attivare_plus_con_punti']='I punti servono per poter avere un Account Plus per tot tempo: 3 punti= 1 settimana, 10 punti= 1 mese e 100= 1 anno';
 $lang['non_puoi_attivare_nessun_plus']='Non hai abbastanza punti per attivare nessun tipo di Account Plus';
-$lang['attiva_plus_con_punti1']='Attiva plus di 1 settimana';
-$lang['attiva_plus_con_punti2']='Attiva plus di 1 mese';
-$lang['attiva_plus_con_punti3']='Attiva plus di 1 anno';
+$lang['attiva_plus_con_punti1']='Attiva Account plus di 1 settimana';
+$lang['attiva_plus_con_punti2']='<br />Attiva Account plus di 1 mese';
+$lang['attiva_plus_con_punti3']='<br />Attiva Account plus di 1 anno';
 $lang['opzioni_error2']='- Non hai abbastanza punti';
 $lang['desc_link_refer']='Gli utenti che visiteranno %s ed entro 1 settimana si iscriveranno e saranno ben attivi, ti faranno guadagnare un punto Plus e, se continueranno a giocare normalmente, guadagnerai 1 punto Plus ogni mese. Attualmente, tale sistema è valido solo per l\'iscrizione allo stesso server. Nel tuo caso &egrave; il server %s';
 ?>
