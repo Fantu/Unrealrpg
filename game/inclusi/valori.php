52c8003896d2513523f492034b3a4dc88b7da953
Fabio Fantoni <fantonifabio@tiscali.it>
2008-02-15 22:24:09 +0000

    modificato tempo di attesa fra i lavori

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,10 +1,10 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="0.5.2 R.592";
+$game_revision="0.5.2";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV");
 $game_se_code="f87d5gf945fhut";
-$game_proxlav_normal=21600;
-$game_proxlav_plus=18000;
+$game_proxlav_normal=10800;
+$game_proxlav_plus=7200;
 ?>
