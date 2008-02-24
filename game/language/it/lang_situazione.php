eaea9cd523c5453fd1262465b9017980db55583f
Fabio Fantoni <fantonifabio@tiscali.it>
2008-02-24 15:36:29 +0000

    miglioramenti

--- game/language/it/lang_situazione.php
+++ game/language/it/lang_situazione.php
@@ -5,5 +5,5 @@ $lang['nuovi_msg']='Hai %s nuovi messaggi';
 $lang['prossimo_lavoro']='Il prossimo lavoro lo potrai svolgere: ';
 $lang['stai_gia_lavorando']='Stai gi&agrave; lavorando';
 $lang['news']='Novit&agrave;: ';
-$lang['comunicazione']='Comunicazione: ';
+$lang['comunicazione']='Comunicazioni: ';
 ?>
