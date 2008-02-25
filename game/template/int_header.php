95465a7b7cdbf6645cf6c63d97724a3c81880717
Fabio Fantoni <fantonifabio@tiscali.it>
2008-02-25 17:08:03 +0000

    modificata la codifica a unicode

--- game/template/int_header.php
+++ game/template/int_header.php
@@ -6,7 +6,7 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo $game_name; echo" "; echo $game_version; ?></title>
 <?php include('inclusi/meta.php'); ?>
 <link href="template/stile.css" rel="stylesheet" type="text/css" title="all"></link>
--- index.php
+++ index.php
@@ -22,7 +22,7 @@ exit();}
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo $game_name; echo" "; echo $game_version; echo" "; echo $game_revision; ?></title>
 <?php include('game/inclusi/meta.php'); ?>
 <script type="text/javascript">
