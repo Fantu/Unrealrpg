6fa55655347036bd643d0600a8625799c21851d4
Fabio Fantoni <fantonifabio@tiscali.it>
2008-03-05 16:40:39 +0000

    fix

--- game/template/int_libro.php
+++ game/template/int_libro.php
@@ -9,8 +9,8 @@ if($semagie!=1){echo $lang['nessuna_magia_presente'];}else{
 foreach($elementi as $chiaveel=>$elementoel){ 
 echo $elementoel."<br />";
 foreach($tipimagia as $chiavetm=>$elementotm){echo $elementotm."<br />";
-foreach($magie as $elemento){
-if($magie[$elemento['magid']]['tipo']==$elementotm AND $magie[$elemento['magid']]['elemento']==$elementoel){
+foreach($magie as $chiave=>$elemento){
+if($magie[$elemento['magid']]['tipo']==$chiavetm AND $magie[$elemento['magid']]['elemento']==$chiaveel){
 echo $lang['magia'.$elemento['magid']]." - ".$lang['dmagia'.$elemento['magid']];
 }//se corrispondente
 }//mostra ogni magia
