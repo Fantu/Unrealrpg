434cd0df8426924c0d4190338ddc1cd3c759d918
Fabio Fantoni <fantonifabio@tiscali.it>
2008-03-08 15:28:35 +0000

    miglioramento

--- game/inclusi/funzioni_eventi.php
+++ game/inclusi/funzioni_eventi.php
@@ -334,9 +334,9 @@ $testo2="<span>".sprintf($lang['report_esplosione_lab2'],$danni)."</span>";
 $titolo=$lang['report_esplosione_laboratorio'];
 $db->QueryMod("INSERT INTO messaggi (userid,titolo,testo,mittenteid,data) VALUES ('".$userid."','".$titolo."','".$testo2."','0','".$adesso."')");	
 $testo3=$lang['report_lavlab_pozione_no']."<br />";
-$exp=floor($exp/2);
+$exp-=floor($exp/2);
 }/*fine esplosione*/else{
-$exp=$exp*2;
+$exp+=floor($exp*2);
 $db->QueryMod("INSERT INTO inoggetti (oggid,userid) VALUES ('".$pozione['id']."','".$userid."')");
 $nomepozione=$lang['oggetto'.$pozione['id'].'_nome'];
 $testo3=sprintf($lang['report_lavlab_pozione_si'],$nomepozione)."<br />";
