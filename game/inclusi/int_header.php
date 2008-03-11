50551d343730612e52c9e12679e9077ea6bbfff5
Fabio Fantoni <fantonifabio@tiscali.it>
2008-03-11 22:21:26 +0000

    miglioramento

--- game/inclusi/int_header.php
+++ game/inclusi/int_header.php
@@ -41,7 +41,7 @@ if ($adesso>($usercar['decfede']+3600)){
 	if ($usercar['fede']>0){
 		$differenzaora=$adesso-$usercar['decfede'];
 		$ore=floor($differenzaora/3600);
-		$fede=$usercar['fede']-($ore*20);
+		$fede=$usercar['fede']-($ore*10);
 		if ($fede<0)
 		$fede=0;
 		$db->QueryMod("UPDATE caratteristiche SET decfede=decfede+'".($ore*3600)."',fede='".($fede)."' WHERE userid='".$user['userid']."'");
