7ba9289e4db83ff1024096ab8e174748e6285bf5
Fabio Fantoni <fantonifabio@tiscali.it>
2008-03-20 14:13:53 +0000

    miglioramento

--- admin/admin_server.php
+++ admin/admin_server.php
@@ -8,22 +8,20 @@ $db->database=$chiave;
 $db->QueryMod("UPDATE config SET chiuso='1' WHERE id='".$chiave."' LIMIT 1");
 echo sprintf($lang['server_chiuso'],$chiave);
 }//ogni server
-}elseif(isset($_POST["apritutti"])) {
+}
+if(isset($_POST["apritutti"])) {
 foreach($game_server as $chiave=>$elemento){
 $db->database=$chiave;
 $db->QueryMod("UPDATE config SET chiuso='0' WHERE id='".$chiave."' LIMIT 1");
-echo sprintf($lang['server_aperto'],$chiave);
+echo sprintf($lang['server_aperto'],$chiave)."<br/>";
 }//ogni server
-}else{
 ?>
- 	<form method="post" name="fchiuditutti">
-      <input type="submit" value="<?php echo $lang['chiudi_tutti_server']; ?>" name="chiuditutti" />
-    </form>
-    <br/><br/>
-    <form method="post" name="fapritutti">
-      <input type="submit" value="<?php echo $lang['apri_tutti_server']; ?>" name="apritutti" />
-    </form>
-    <br/><br/>
-<?php
-}//mostra form
-?>
+<br/><br/>
+<form method="post" name="fchiuditutti">
+<input type="submit" value="<?php echo $lang['chiudi_tutti_server']; ?>" name="chiuditutti" />
+</form>
+<br/><br/>
+<form method="post" name="fapritutti">
+<input type="submit" value="<?php echo $lang['apri_tutti_server']; ?>" name="apritutti" />
+</form>
+<br/><br/>
