4f5c78811428355fac9dda2421bfb6f36162e231
Fabio Fantoni <fantonifabio@tiscali.it>
2008-03-22 10:31:53 +0000

    fix

--- game/index.php
+++ game/index.php
@@ -48,7 +48,7 @@ if ($user['personaggio']==0){
 	require('creapersonaggio.php');	}
 	else{
 		$location=htmlspecialchars($_GET['loc'],ENT_QUOTES);
-		if(!in_array($location.'.php'),$game_location)
+		if(!in_array($location.'.php',$game_location))
 		$location="situazione";
 		require($location.'.php');
 		}
