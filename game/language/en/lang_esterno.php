a44ae0b183c9ccd61fa4977381545d57993f1b63
Fabio Fantoni <fantonifabio@tiscali.it>
2008-03-27 15:53:23 +0000

    aggiornati file lingua

--- game/language/en/lang_esterno.php
+++ game/language/en/lang_esterno.php
@@ -17,22 +17,23 @@ $lang['Registrati']='Register';
 $lang['outputerrori']='The following errors have occured:';
 $lang['account_creato_ok']='<strong>Account creato con successo!!</strong><br />Prima di poter iniziare a giocare, dovrai confermare l`iscrizione visitando il link contenuto nella mail che ti è stata inviata all`indirizzo di posta inserito.<br />Se non trovi la mail, controlla nelle cartelle Posta indesiderata, Antispam o simili.<br /><br />';
 $lang['Conferma_account']='Confirm account ';
-$lang['testo_mail_conferma']='Ciao,<br />Per confermare l\'iscrizione a %s devi visitare il link sottostante:<br /> <a href="%s/conferma.php?t=%s&cod=%s">Conferma account</a> <br /><br />Finch&egrave; l\'account non verr&agrave; confermato, non potrai accedere al gioco.<br />Saluti,<br />%s Staff';
+$lang['testo_mail_conferma']='Ciao,<br />Per confermare l`iscrizione a %s devi visitare il link sottostante:<br /> <a href="%s/conferma.php?t=%s&cod=%s">Conferma account</a> <br /><br />Finchè l`account non verrà confermato, non potrai accedere al gioco.<br />Saluti,<br />%s Staff';
 $lang['altre_lingue']='Le altre lingue in cui è disponibile %s sono: ';
 
 $lang['reg_error1']='- Non hai selezionato il server!<br />';
 $lang['reg_error2']='- Non hai scritto il tuo username!<br />';
-$lang['reg_error3']='- L\'username deve essere almeno di 3 caratteri!<br />';
+$lang['reg_error3']='- L`username deve essere almeno di 3 caratteri!<br />';
 $lang['reg_error4']='- Non hai scritto la password!<br />';
 $lang['reg_error5']='- The password has to be at least 6 characters long.<br />';
 $lang['reg_error6']='- Non hai inserito l`indirizzo email!<br />';
-$lang['reg_error7']='- L\'indirizzo email inserito non sembra essere corretto.<br />';
-$lang['reg_error8']='- L\'username che hai scelto &egrave; gi&agrave; stato utilizzato!<br />';
+$lang['reg_error7']='- L`indirizzo email inserito non sembra essere corretto.<br />';
+$lang['reg_error8']='- L`username che hai scelto è già stato utilizzato!<br />';
 $lang['reg_error9']='- This server is full at the moment, please choose another server.<br />';
 
 $lang['ext_error1']='Login errato!\\nUsername e/o password non corretti.';
 $lang['ext_error2']='Login impossibile!\\nQuesto account non è ancora stato confermato.';
 $lang['ext_error3']='Tempo limite scaduto!\\nNuovo login necessario.';
+$lang['ext_error4']='Login fallito\\nPassword errata!';
 $lang['ext_error5']='Impossibile confermare questo account!\\nLink errato, controlla e riprova.';
 $lang['ext_error6']='Impossibile confermare questo account!\\nQuesto account è già stato confermato.';
 $lang['ext_error7']='Account confermato!\\nAdesso puoi entrare e iniziare a giocare.';
