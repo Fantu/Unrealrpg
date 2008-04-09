12eb9fff0cb8b4d6f97e48025831b6350e0def20
Fabio Fantoni <fantonifabio@tiscali.it>
2008-04-09 06:41:28 +0000

    miglioramenti

--- game/language/it/lang_laboratorio.php
+++ game/language/it/lang_laboratorio.php
@@ -1,8 +1,8 @@
 <?php
 $lang['lav_apprendista']='Lavora come apprendista';
-$lang['desc_lav_apprendista']='Puoi lavorare in Laboratorio come apprendista, avendo una retribuzione fissa di 6 monete ad ora e guadagnare esperienza per l\'abilit&agrave; di alchimista, utile per avere risultati migliori con meno fatica e meno danni.';
+$lang['desc_lav_apprendista']='Puoi lavorare in Laboratorio come apprendista, avendo una retribuzione fissa di 6 monete ad ora e guadagnare esperienza per l\'abilità di alchimista, utile per avere risultati migliori con meno fatica e meno danni.';
 $lang['lab_errore1']='- Non hai sufficienti energie per poter lavorare in Laboratorio.<br />';
-$lang['lab_errore2']='- Attualmente il tuo fisico non pu&ograve; reggere il lavoro in Laboratorio.<br />';
+$lang['lab_errore2']='- Attualmente il tuo fisico non può reggere il lavoro in Laboratorio.<br />';
 $lang['lab_errore3']='- Devono passare 3 ore per ogni ora di lavoro dall`ultimo effettuato.<br />';
 $lang['lab_errore4']='- Hai troppo poco mana!<br />';
 $lang['lab_errore5']='- Non hai abbastanza ampolle per poter effettuare esperimenti o distillare pozioni.<br />';
@@ -12,13 +12,14 @@ $lang['lab_errore8']='- Non hai selezionato la pozione da creare!<br />';
 $lang['report_lavlabapp']='Hai lavorato in Laboratorio come apprendista per 1 ora, guadagnando %s monete e %s di esperienza. Il lavoro ti ha stancato (hai perso %s di energia), il tuo fisico ne ha risentito (hai perso %s di salute) e hai consumato %s di mana.';
 $lang['report_lavoro_labapp']='Completato lavoro in Laboratorio come apprendista.';
 $lang['report_esplosione_laboratorio']='Imprevisto durante il lavoro in Laboratorio!';
-$lang['report_esplosione_lab1']='Durante il tuo lavoro in Laboratorio &egrave; esplosa una pozione ma sei riuscito ad allontanarti in tempo e non ferirti.';
-$lang['report_esplosione_lab2']='Durante il tuo lavoro in Laboratorio &egrave; esplosa una pozione, non sei riuscito ad allontanarti in tempo e ti sei ferito (%s di salute persa).';
+$lang['report_esplosione_lab1']='Durante il tuo lavoro in Laboratorio è esplosa una pozione ma sei riuscito ad allontanarti in tempo e non ferirti.';
+$lang['report_esplosione_lab2']='Durante il tuo lavoro in Laboratorio è esplosa una pozione, non sei riuscito ad allontanarti in tempo e ti sei ferito (%s di salute persa).';
 $lang['lav_alchimista']='Lavora come alchimista';
-$lang['desc_lav_alchimista']='Puoi lavorare in Laboratorio come alchimista cercando di distillare delle pozioni, alcune delle quali, potranno essere create solo in base alla tua abilit&agrave;, non avendo per&ograve; sicurezza circa la loro riuscita.';
+$lang['desc_lav_alchimista']='Puoi lavorare in Laboratorio come alchimista cercando di distillare delle pozioni, alcune delle quali, potranno essere create solo in base alla tua abilità, non avendo però sicurezza circa la loro riuscita.';
 $lang['nessuna_pozione_poss']='Non sei abbastanza esperto per distillare nessuna pozione.';
 $lang['report_lavlabalc']='Hai lavorato in Laboratorio come alchimista per 1 ora, guadagnando %s di esperienza. Il lavoro ti ha stancato (hai perso %s di energia), il tuo fisico ne ha risentito (hai perso %s di salute) e hai consumato %s di mana.<br />Hai speso %s monete per comprare gli ingredienti necessari.';
 $lang['report_lavoro_labalc']='Completato il lavoro in Laboratorio come alchimista.';
 $lang['report_lavlab_pozione_no']='Non sei riuscito a distillare la pozione voluta.';
 $lang['report_lavlab_pozione_si']='Sei riuscito a distillare %s.';
+$lang['seleziona_pozione']='Seleziona quale pozione:';
 ?>
