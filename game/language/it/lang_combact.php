07825503948045d9b1190a9959cc8d6b21bbc80d
Fabio Fantoni <fantonifabio@tiscali.it>
2008-04-15 17:22:38 +0000

    miglioramenti

--- game/language/it/lang_combact.php
+++ game/language/it/lang_combact.php
@@ -10,7 +10,7 @@ $lang['rispondi_sfida']='%s ti vuole sfidare';
 $lang['sfida_rifiutata']='Sfida rifiutata';
 $lang['report_sfida_rifiutata']='La sfida è stata rifiutata.';
 
-$lang['danno_att_vicino']='%s attacca %s con %s e lo colpisce.';
+$lang['danno_att_vicino']='%s attacca %s con %s e lo colpisce infliggendo %s di danno.';
 $lang['niente_att_vicino']='%s attacca %s con %s senza riuscire a colpirlo.';
 $lang['pugno']='pugno';
 ?>
