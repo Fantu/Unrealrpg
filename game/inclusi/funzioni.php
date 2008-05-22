6692ac82ab6e5046617f3acd3a5a6cc56c8d654a
Fabio Fantoni <fantonifabio@tiscali.it>
2008-05-22 17:28:54 +0000

    miglioramenti

--- game/inclusi/funzioni.php
+++ game/inclusi/funzioni.php
@@ -1,5 +1,43 @@
 <?php
-function redirect($doveredirettare) {
-header($doveredirettare);
+function testosalute($percsalute){
+if ($percsalute<1){
+$salute=$lang['morto'];
+}elseif ($percsalute>=1 AND $percsalute<10){
+$salute=$lang['pessima'];
+}elseif ($percsalute>=10 AND $percsalute<20){
+$salute=$lang['molto_bassa'];
+}elseif ($percsalute>=20 AND $percsalute<40){
+$salute=$lang['bassa'];
+}elseif ($percsalute>=40 AND $percsalute<60){
+$salute=$lang['media'];
+}elseif ($percsalute>=60 AND $percsalute<80){
+$salute=$lang['alta'];
+}elseif ($percsalute>=80 AND $percsalute<90){
+$salute=$lang['molto_alta'];
+}elseif ($percsalute>=90){
+$salute=$lang['perfetta'];
 }
+return $salute;
+}//fine testosalute
+
+function testoenergia($percenergia){
+if ($percenergia<5){
+$energia=$lang['esausto'];
+}elseif ($percenergia>=5 AND $percenergia<10){
+$energia=$lang['pessima'];
+}elseif ($percenergia>=10 AND $percenergia<20){
+$energia=$lang['molto_bassa'];
+}elseif ($percenergia>=20 AND $percenergia<40){
+$energia=$lang['bassa'];
+}elseif ($percenergia>=40 AND $percenergia<60){
+$energia=$lang['media'];
+}elseif ($percenergia>=60 AND $percenergia<80){
+$energia=$lang['alta'];
+}elseif ($percenergia>=80 AND $percenergia<90){
+$energia=$lang['molto_alta'];
+}elseif ($percenergia>=90){
+$energia=$lang['perfetta'];
+}
+return $energia;
+}//fine testoenergia
 ?>
--- game/visualizzautente.php
+++ game/visualizzautente.php
@@ -7,41 +7,9 @@ require('inclusi/personaggio.php');
 $utente=(int)$_GET['id'];
 $datiutente=$db->QuerySelect("SELECT t1.userid AS userid,t1.username AS username,t1.ultimazione AS ultimazione,t2.livello AS livello,t2.razza AS razza,t2.classe AS classe,t2.sesso AS sesso,t2.salute AS salute,t2.energiamax AS energiamax,t2.saluteattuale AS saluteattuale,t2.energia AS energia,t2.reputazione AS reputazione FROM utenti AS t1 JOIN caratteristiche t2 ON t1.userid=t2.userid WHERE t1.userid='".$utente."' LIMIT 1");
 $percsalute=100/$datiutente['salute']*$datiutente['saluteattuale'];
-if ($percsalute<1){
-$salute=$lang['morto'];
-}elseif ($percsalute>=1 AND $percsalute<10){
-$salute=$lang['pessima'];
-}elseif ($percsalute>=10 AND $percsalute<20){
-$salute=$lang['molto_bassa'];
-}elseif ($percsalute>=20 AND $percsalute<40){
-$salute=$lang['bassa'];
-}elseif ($percsalute>=40 AND $percsalute<60){
-$salute=$lang['media'];
-}elseif ($percsalute>=60 AND $percsalute<80){
-$salute=$lang['alta'];
-}elseif ($percsalute>=80 AND $percsalute<90){
-$salute=$lang['molto_alta'];
-}elseif ($percsalute>=90){
-$salute=$lang['perfetta'];
-}
+$salute=testosalute($percsalute);
 $percenergia=100/$datiutente['energiamax']*$datiutente['energia'];
-if ($percenergia<5){
-$energia=$lang['esausto'];
-}elseif ($percenergia>=5 AND $percenergia<10){
-$energia=$lang['pessima'];
-}elseif ($percenergia>=10 AND $percenergia<20){
-$energia=$lang['molto_bassa'];
-}elseif ($percenergia>=20 AND $percenergia<40){
-$energia=$lang['bassa'];
-}elseif ($percenergia>=40 AND $percenergia<60){
-$energia=$lang['media'];
-}elseif ($percenergia>=60 AND $percenergia<80){
-$energia=$lang['alta'];
-}elseif ($percenergia>=80 AND $percenergia<90){
-$energia=$lang['molto_alta'];
-}elseif ($percenergia>=90){
-$energia=$lang['perfetta'];
-}
+$energia=testosalute($percenergia);
 $cheordine=htmlspecialchars($_GET['ordine'],ENT_QUOTES);
 $iniziale=(int)$_GET['inizio'];
 $linkindietro="index.php?loc=utenti&amp;ordine=".$cheordine."&amp;inizio=".$iniziale;
