89b3d0abe8b5b19c9b04cfa4eb1fd5bcbbfbeda7
Fabio Fantoni <fantonifabio@tiscali.it>
2008-05-23 16:38:12 +0000

    miglioramento

--- pagine/index.php
+++ pagine/index.php
@@ -54,7 +54,7 @@ include('pagine/home.php');
 <div align="center">
 <br /><br />
 <?php echo $game_name; ?> &copy; 2007 Powered by <a href="http://www.lostgames.net" target="_blank">Lostgames.net</a>
-<br />
+<br /><br />
 <a href="http://validator.w3.org/check?uri=referer" target="_blank">
 	<img id="xhtml" src="game/template/immagini/xhtml_grigio.gif" alt="" border="0" onmouseover="CambiaImg('xhtml', true);" onmouseout="CambiaImg('xhtml', false);" />
 </a>&nbsp;
@@ -66,7 +66,8 @@ include('pagine/home.php');
 </a>&nbsp;&nbsp;
 <a href="http://www.mysql.com" target="_blank">
 	<img id="mysql" src="game/template/immagini/mysql_grigio.gif" alt="" border="0" onmouseover="CambiaImg('mysql', true);" onmouseout="CambiaImg('mysql', false);" />
-</a>&nbsp;&nbsp;<br /><div id="tempogenpag">
+</a>&nbsp;&nbsp;<br /><br />
+<div id="tempogenpag">
 <?php
 if($_GET['error']){
 require("game/inclusi/errori.php");}
