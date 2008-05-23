fae940f84f08889e4c908945cddb7371dab68866
Fabio Fantoni <fantonifabio@tiscali.it>
2008-05-23 16:13:54 +0000

    prova grafica

--- game/template/int_footer.php
+++ game/template/int_footer.php
@@ -3,7 +3,7 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	header("Location: ../../index.php?error=16");
 	exit();
 }?>
-<br /><div align="center" id="tempogenpag">
+<br /><div id="tempogenpag">
 <?php
 $end_time=time()+microtime();
 $gen_time=number_format($end_time-$start_time, 4, '.', '');
