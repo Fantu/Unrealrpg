8c1255e880d579aed2fc74909c4076da258e5a02
Fabio Fantoni <fantonifabio@tiscali.it>
2008-05-23 14:27:33 +0000

    prova grafica

--- game/template/int_menu.php
+++ game/template/int_menu.php
@@ -4,63 +4,23 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	exit();
 }?>
 <div id="menu">
-<table width="142"  border="1" cellspacing="1" cellpadding="1" class="tabmenu">		  
-	<tr>
-    <td><div align="center"><a href="index.php?loc=situazione"><?php echo $lang['Situazione']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=banca"><?php echo $lang['Banca']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=miniera"><?php echo $lang['Miniera']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=laboratorio"><?php echo $lang['Laboratorio']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=tempio"><?php echo $lang['Tempio']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=fucina"><?php echo $lang['Fucina']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=rocca"><?php echo $lang['Rocca_arcano']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=libro"><?php echo $lang['Libro_incantesimi']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=combact"><?php echo $lang['Combattimenti']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=mercato"><?php echo $lang['Mercato']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=inventario"><?php echo $lang['Inventario']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=equipaggiamento"><?php echo $lang['Equipaggiamento']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=messaggi"><?php echo $lang['Messaggi']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=utenti"><?php echo $lang['Lista_utenti']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=guida"><?php echo $lang['Guida']; ?></a></div></td>
-	</tr>				
-	<tr>
-    <td><div align="center"><a href="index.php?loc=changelog"><?php echo $lang['Changelog']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=opzioni"><?php echo $lang['Opzioni']; ?></a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="http://www.lostgames.net/forum/forumdisplay.php?f=33" target="_blank">Forum</a></div></td>
-	</tr>
-	<tr>
-    <td><div align="center"><a href="index.php?loc=logout"><?php echo $lang['Logout']; ?></a></div></td>
-	</tr>		
-</table>
+<a href="index.php?loc=situazione"><?php echo $lang['Situazione']; ?></a>
+<a href="index.php?loc=banca"><?php echo $lang['Banca']; ?></a>
+<a href="index.php?loc=miniera"><?php echo $lang['Miniera']; ?></a>
+<a href="index.php?loc=laboratorio"><?php echo $lang['Laboratorio']; ?></a>
+<a href="index.php?loc=tempio"><?php echo $lang['Tempio']; ?></a>
+<a href="index.php?loc=fucina"><?php echo $lang['Fucina']; ?></a>
+<a href="index.php?loc=rocca"><?php echo $lang['Rocca_arcano']; ?></a>
+<a href="index.php?loc=libro"><?php echo $lang['Libro_incantesimi']; ?></a>
+<a href="index.php?loc=combact"><?php echo $lang['Combattimenti']; ?></a>
+<a href="index.php?loc=mercato"><?php echo $lang['Mercato']; ?></a>
+<a href="index.php?loc=inventario"><?php echo $lang['Inventario']; ?></a>
+<a href="index.php?loc=equipaggiamento"><?php echo $lang['Equipaggiamento']; ?></a>
+<a href="index.php?loc=messaggi"><?php echo $lang['Messaggi']; ?></a>
+<a href="index.php?loc=utenti"><?php echo $lang['Lista_utenti']; ?></a>
+<a href="index.php?loc=guida"><?php echo $lang['Guida']; ?></a>
+<a href="index.php?loc=changelog"><?php echo $lang['Changelog']; ?></a>
+<a href="index.php?loc=opzioni"><?php echo $lang['Opzioni']; ?></a>
+<a href="http://www.lostgames.net/forum/forumdisplay.php?f=33" target="_blank">Forum</a>
+<a href="index.php?loc=logout"><?php echo $lang['Logout']; ?></a>
 </div>
