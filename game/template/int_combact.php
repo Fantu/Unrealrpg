362bde8fcad8211b252d77e0b243b8b95aef76f8
Fabio Fantoni <fantonifabio@tiscali.it>
2008-05-26 10:44:41 +0000

    miglioramento

--- game/template/int_combact.php
+++ game/template/int_combact.php
@@ -10,7 +10,7 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 <?php echo $outputsfida; ?>
 <br />
 <br />
-<div align="center"><?php if($combactview==1) echo $titleoutputcombact; ?></div><br />
 <br />
+<div align="center"><?php if($combactview==1) echo $titleoutputcombact; ?></div><br />
 <?php echo $outputcombact; ?>
 <br />
