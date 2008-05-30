2179ccdeec82ab3a27a8f2df2e24e67dc542e6f5
Fabio Fantoni <fantonifabio@tiscali.it>
2008-05-30 16:02:36 +0000

    miglioramento

--- game/opzioni.php
+++ game/opzioni.php
@@ -55,12 +55,9 @@ $newsletters=$lang['newsletter_disattiva']." <a href=\"index.php?loc=opzioni&amp
 else
 $newsletters=$lang['newsletter_attiva']." <a href=\"index.php?loc=opzioni&amp;newsletter=0\">".$lang['disattiva']."</a>";
 
-if($_GET['newsletter']){
 $news=(int)$_GET['newsletter'];
-if($news!=0)
-$news=1;
+if($news!=$user['mailnews'])
 $db->QueryMod("UPDATE utenti SET mailnews='".$news."' WHERE userid='".$user['userid']."' LIMIT 1");
-}
 
 require('inclusi/personaggio.php');
 require('template/int_opzioni.php');
