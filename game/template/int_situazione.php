f4c50999c3ee94ae132a214c97bec7dbd89e9f48
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-01 10:14:50 +0000

    prova

--- game/template/int_situazione.php
+++ game/template/int_situazione.php
@@ -44,8 +44,7 @@ echo $usercar['livello'];
 <?php echo $lang['Destrezza']; ?>: <?php echo $usercar['destrezza']; ?><br />
 <br />
 <?php echo $lang['elenco_ab']; ?><br />
-<?php echo $lang['ab_minatore']; ?>: <?php echo $usercar['minatore']; ?>
-<table align="center" border="0" cellspacing="0"><tr><td width="<?php echo $percmin1; ?>" class="sfondoverde"></td><td width="<?php echo $percmin2; ?>" class="sfondorosso"></td></tr>
+<table align="center" border="0" cellspacing="0"><tr><td><?php echo $lang['ab_minatore']; ?>: <?php echo $usercar['minatore']; ?></td><td width="<?php echo $percmin1; ?>" class="sfondoverde"></td><td width="<?php echo $percmin2; ?>" class="sfondorosso"></td></tr>
 </table>
 <?php echo $lang['ab_alchimista']; ?>: <?php echo $usercar['alchimista']; ?>
 <table align="center" border="0" cellspacing="0"><tr><td width="<?php echo $percmin3; ?>" class="sfondoverde"></td><td width="<?php echo $percmin4; ?>" class="sfondorosso"></td></tr>
