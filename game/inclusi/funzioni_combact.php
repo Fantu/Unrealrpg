d1db17fb9906b173b77565c5d3d497b8a8a1b759
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-02 09:29:30 +0000

    fix

--- game/inclusi/funzioni_combact.php
+++ game/inclusi/funzioni_combact.php
@@ -244,12 +244,12 @@ if($dc->tattica(1,1)==2 AND $dc->tattica(2,1)==2){//se entrambi si arrendono
 $input=$lang['finito_entrambi_arresi']."<br/>";
 }elseif($dc->tattica(1,1)==2){//se il primo si arrende
 $input=sprintf($lang['finito_resa'],$dc->nome(1),$dc->nome(2))."<br/>";
-$rep=Checkrep(1);
+$rep=$dc->Checkrep(1);
 if($rep[0]==2)
 $db->QueryMod("UPDATE caratteristiche SET reputazione=reputazione-'".$rep[1]."' WHERE userid='".$this->id(1)."' LIMIT 1");
 }elseif($dc->tattica(2,1)==2){//se il secondo si arrende
 $input=sprintf($lang['finito_resa'],$dc->nome(2),$dc->nome(1))."<br/>";
-$rep=Checkrep(2);
+$rep=$dc->Checkrep(2);
 if($rep[0]==2)
 $db->QueryMod("UPDATE caratteristiche SET reputazione=reputazione-'".$rep[1]."' WHERE userid='".$this->id(2)."' LIMIT 1");
 }elseif($dc->esausto(1)==1 AND $dc->esausto(2)==1){//se entrambi esausti
