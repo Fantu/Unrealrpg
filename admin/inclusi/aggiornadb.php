f2010abf1598c5592222244ba6feba7a59914449
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-08 13:30:50 +0000

    aggiunta

--- admin/inclusi/aggiornadb.php
+++ admin/inclusi/aggiornadb.php
@@ -3,29 +3,35 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	header("Location: ../../index.php?error=16");
 	exit();
 }
-$newversion="0.6.8";
+$newversion="0.6.9";
 foreach($game_server as $chiave=>$elemento){
 if($chiave!=999){
 $db->database=$chiave;
 $check=$db->QuerySelect("SELECT version FROM config WHERE id=".$chiave);
 if($check['version']!=$newversion AND $newversion==$game_revision){
-$db->QueryMod("CREATE TABLE `equip` (
-`id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-`oggid` SMALLINT UNSIGNED NOT NULL ,
-`userid` SMALLINT UNSIGNED NOT NULL ,
-`usura` SMALLINT UNSIGNED NOT NULL DEFAULT '0',
-`inuso` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0',
-PRIMARY KEY ( `id` )
-) ENGINE = MYISAM");
-$b=$db->QuerySelect("SELECT count(id) AS num FROM inoggetti WHERE equip='1'");
-if($b['num']>0){
-$a=$db->QueryCiclo("SELECT * FROM inoggetti WHERE equip='1'");
-while($var=$db->QueryCicloResult($a))
-{
-	$db->QueryMod("DELETE FROM inoggetti WHERE id='".$var['id']."' LIMIT 1");
-	$db->QueryMod("INSERT INTO equip (oggid,userid,usura) VALUES ('".$var['oggid']."','".$var['userid']."','".$var['usura']."')");
-}//per ogni ogg equip
-}//se ci sono oggetti equip
+$db->QueryMod("ALTER TABLE `oggetti` ADD `difesafisica` SMALLINT UNSIGNED NOT NULL DEFAULT '0'");
+$db->QueryMod("INSERT INTO `oggetti` (
+`id` ,
+`tipo` ,
+`categoria` ,
+`probrottura` ,
+`costo` ,
+`energia` ,
+`usura` ,
+`bonuseff` ,
+`forzafisica` ,
+`probtrovare` ,
+`recsalute` ,
+`recenergia` ,
+`abilitanec` ,
+`materiale` ,
+`danno` ,
+`difesafisica`
+)
+VALUES 
+(NULL , '6', '1', '200', '10', '5', '20', '0', '8', '0', '0', '0', '0', '0', '0', '5'),
+(NULL , '6', '1', '150', '20', '6', '50', '0', '10', '0', '0', '0', '0', '0', '0', '8');
+");
 	/*//creazione record per tab con 1 record per utente
 	$a=$db->QueryCiclo("SELECT userid FROM utenti WHERE conferma='1' AND personaggio='1'");
 	while($var=$db->QueryCicloResult($a))
