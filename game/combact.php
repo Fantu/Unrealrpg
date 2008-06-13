a0212f6d68c61ace9924d7e34f556b50065a9aa2
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-13 09:10:02 +0000

    miglioramento

--- game/combact.php
+++ game/combact.php
@@ -7,6 +7,55 @@ require_once('language/'.$language.'/lang_combact.php');
 require_once('inclusi/funzioni_combact.php');
 $do=htmlspecialchars($_GET['do'],ENT_QUOTES);
 $combactview=0;
+
+$outputsfida=$lang['nessuna_sfida'];
+if ($eventi['id']>0){
+$eventisfida=$db->QuerySelect("SELECT * FROM eventi WHERE userid='".$user['userid']."' LIMIT 1");
+if($eventisfida['tipo']==4 AND $eventisfida['type']==2){
+$sfidante=$db->QuerySelect("SELECT username FROM utenti WHERE userid='".$eventisfida['oggid']."'");
+$outputsfida=sprintf($lang['rispondi_sfida'],$sfidante['username'])." - <a href=\"index.php?loc=combact&amp;do=rispsfida&amp;risp=0\">".$lang['Rifiuta']."</a> - <a href=\"index.php?loc=combact&amp;do=rispsfida&amp;risp=1\">".$lang['Accetta']."</a>";
+}elseif($eventisfida['tipo']==5){
+$filerep="inclusi/log/report/".$db->database."/".$eventisfida['battleid'].".log";
+ob_start();
+include $filerep;
+$report=ob_get_contents();
+ob_end_clean();
+$combactview=2;
+$titleoutputcombact=$lang['titolo_report_combattimento2'];
+$outputcombact="<table width=\"500\" align=\"center\">".$report."</table>";
+$batt=$db->QuerySelect("SELECT * FROM battle WHERE id='".$eventisfida['battleid']."' LIMIT 1");
+$tattica=(int)$_GET['tattica'];
+$subtattica=(int)$_GET['subtatt'];
+if($tattica!=0){
+if($batt['attid']==$user['userid']){
+$db->QueryMod("UPDATE battle SET tatatt='".$tattica."',tatatt2='".$subtattica."' WHERE id='".$eventisfida['battleid']."' LIMIT 1");
+}else{
+$db->QueryMod("UPDATE battle SET tatdif='".$tattica."',tatdif2='".$subtattica."' WHERE id='".$eventisfida['battleid']."' LIMIT 1");
+}
+}//fine imposta tattica
+$batt=$db->QuerySelect("SELECT * FROM battle WHERE id='".$eventisfida['battleid']."' LIMIT 1");
+if($batt['attid']==$user['userid']){
+$tattica=$batt['tatatt'];
+$subtattica=$batt['tatatt2'];
+}else{
+$tattica=$batt['tatdif'];
+$subtattica=$batt['tatdif2'];
+}
+switch($tattica){
+case 0:
+$viewtattic=$lang['nessuna'];
+break;//fine nessuna
+case 1:
+$viewtattic=$lang['tattica_attacco']." ".$lang['tattica_attacco_cac'];
+break;//fine attacco
+case 2:
+$viewtattic=$lang['tattica_resa'];
+break;//fine resa
+}
+$viewtattic=sprintf($lang['tattica_selezionata'],$viewtattic)."<br/>";
+}
+}//se ci sono eventi
+
 switch($do){
 case "sfida":
 $idp=(int)$_GET['id'];
@@ -40,7 +89,6 @@ $idp=(int)$eventisfida['oggid'];
 $db->QueryMod("DELETE FROM eventi WHERE userid='".$user['userid']."'");
 $db->QueryMod("DELETE FROM eventi WHERE userid='".$idp."'");
 if($risp==1){
-//$outputerrori="Sfida accettata ma impossibile procedere con il combattimento...in sviluppo";
 Startcombact($user['userid'],$idp,$user['server']);
 echo "<script language=\"javascript\">window.location.href='index.php?loc=situazione'</script>";
 exit();
@@ -66,57 +114,9 @@ $report=ob_get_contents();
 ob_end_clean();
 $combactview=1;
 $titleoutputcombact=$lang['titolo_report_combattimento1'];
-$outputcombact="<table>".$report."</table>";
+$outputcombact="<table width=\"500\" align=\"center\">".$report."</table>";
 }
 break;//fine sfidda
-}
-
-$outputsfida=$lang['nessuna_sfida'];
-if ($eventi['id']>0){
-$eventisfida=$db->QuerySelect("SELECT * FROM eventi WHERE userid='".$user['userid']."' LIMIT 1");
-if($eventisfida['tipo']==4 AND $eventisfida['type']==2){
-$sfidante=$db->QuerySelect("SELECT username FROM utenti WHERE userid='".$eventisfida['oggid']."'");
-$outputsfida=sprintf($lang['rispondi_sfida'],$sfidante['username'])." - <a href=\"index.php?loc=combact&amp;do=rispsfida&amp;risp=0\">".$lang['Rifiuta']."</a> - <a href=\"index.php?loc=combact&amp;do=rispsfida&amp;risp=1\">".$lang['Accetta']."</a>";
-}elseif($eventisfida['tipo']==5){
-$filerep="inclusi/log/report/".$db->database."/".$eventisfida['battleid'].".log";
-ob_start();
-include $filerep;
-$report=ob_get_contents();
-ob_end_clean();
-$combactview=2;
-$titleoutputcombact=$lang['titolo_report_combattimento2'];
-$outputcombact="<table>".$report."</table>";
-$batt=$db->QuerySelect("SELECT * FROM battle WHERE id='".$eventisfida['battleid']."' LIMIT 1");
-$tattica=(int)$_GET['tattica'];
-$subtattica=(int)$_GET['subtatt'];
-if($tattica!=0){
-if($batt['attid']==$user['userid']){
-$db->QueryMod("UPDATE battle SET tatatt='".$tattica."',tatatt2='".$subtattica."' WHERE id='".$eventisfida['battleid']."' LIMIT 1");
-}else{
-$db->QueryMod("UPDATE battle SET tatdif='".$tattica."',tatdif2='".$subtattica."' WHERE id='".$eventisfida['battleid']."' LIMIT 1");
-}
-}//fine imposta tattica
-$batt=$db->QuerySelect("SELECT * FROM battle WHERE id='".$eventisfida['battleid']."' LIMIT 1");
-if($batt['attid']==$user['userid']){
-$tattica=$batt['tatatt'];
-$subtattica=$batt['tatatt2'];
-}else{
-$tattica=$batt['tatdif'];
-$subtattica=$batt['tatdif2'];
-}
-switch($tattica){
-case 0:
-$viewtattic=$lang['nessuna'];
-break;//fine nessuna
-case 1:
-$viewtattic=$lang['tattica_attacco']." ".$lang['tattica_attacco_cac'];
-break;//fine attacco
-case 2:
-$viewtattic=$lang['tattica_resa'];
-break;//fine resa
-}
-$viewtattic=sprintf($lang['tattica_selezionata'],$viewtattic)."<br/>";
-}
 }
 
 require('template/int_combact.php');
