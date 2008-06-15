db410a560a1fb07aff61ff0e157ffa1b0767e880
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-15 21:29:51 +0000

    fix

--- game/inclusi/funzioni_combact.php
+++ game/inclusi/funzioni_combact.php
@@ -51,9 +51,9 @@ class Dati{
 	$this->che[2]=new Combattente($attaccante,$attn['username'],$attcar,$attequip,$battle['tatatt'],$battle['tatatt2'],$attn['plus']);
 	$this->che[1]=new Combattente($difensore,$difn['username'],$difcar,$difequip,$battle['tatdif'],$battle['tatdif2'],$difn['plus']);
 	}
-	if($dc->plus(1)>0 AND $dc->tattica(1,1)==0)
+	if($this->plus(1)>0 AND $this->tattica(1,1)==0)
 	$this->Autotattic(1);
-	if($dc->plus(2)>0 AND $dc->tattica(2,1)==0)
+	if($this->plus(2)>0 AND $this->tattica(2,1)==0)
 	$this->Autotattic(2);
 	} //fine Stabilisciordine
 	
