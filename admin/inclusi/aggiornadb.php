6168108a7b55ea2e12b676319957acd157eec88a
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-19 10:39:37 +0000

    miglioramento

--- admin/inclusi/aggiornadb.php
+++ admin/inclusi/aggiornadb.php
@@ -3,14 +3,14 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	header("Location: ../../index.php?error=16");
 	exit();
 }
-$newversion="0.6.11";
+$newversion="0.6.12";
 foreach($game_server as $chiave=>$elemento){
 if($chiave!=999){
 $db->database=$chiave;
 $check=$db->QuerySelect("SELECT version FROM config WHERE id=".$chiave);
 if($check['version']!=$newversion AND $newversion==$game_revision){
-$db->QueryMod("ALTER TABLE `equipaggiamento` ADD `scu` SMALLINT UNSIGNED NOT NULL DEFAULT '0'");
-$db->QueryMod("INSERT INTO `oggetti` (
+$db->QueryMod("ALTER TABLE `caratteristiche` CHANGE `saluteattuale` `saluteattuale` MEDIUMINT( 8 ) UNSIGNED NOT NULL DEFAULT '100'");
+/*$db->QueryMod("INSERT INTO `oggetti` (
 `id` ,
 `tipo` ,
 `categoria` ,
@@ -31,7 +31,7 @@ $db->QueryMod("INSERT INTO `oggetti` (
 VALUES 
 (NULL , '6', '2', '200', '10', '12', '20', '0', '22', '0', '0', '0', '0', '0', '0', '8'),
 (NULL , '6', '2', '150', '20', '15', '50', '0', '26', '0', '0', '0', '0', '0', '0', '12');
-");
+");*/
 	/*//creazione record per tab con 1 record per utente
 	$a=$db->QueryCiclo("SELECT userid FROM utenti WHERE conferma='1' AND personaggio='1'");
 	while($var=$db->QueryCicloResult($a))
