50f82e4ed28fc1d5154b511f1c91930ead907aa3
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-24 11:59:11 +0000

    prova

--- game/inclusi/funzioni_combact.php
+++ game/inclusi/funzioni_combact.php
@@ -160,17 +160,20 @@ class Dati{
 	$percsalute2=100/$this->car($chi2,'salute')*$this->car($chi2,'saluteattuale');
 	$percenergia=100/$this->car($chi,'energiamax')*$this->car($chi,'energia');
 	$percenergia2=100/$this->car($chi2,'energiamax')*$this->car($chi2,'energia');
+	$tattiche=array(
+	1=>0,
+	2=>0,
+	3=>0
+	);
 	$tattiche[1]=99;//attacco di base
-	$tattiche['2']=0;
-	$tattiche['3']=0;
 	if($percenergia<20 AND $percsalute<20 AND $percsalute2>40 AND $percenergia2>20){//difesa
-	$tattiche['3']+=100;}
+	$tattiche[3]+=100;}
 	if($percenergia2<5){//se l'avversario è esausto attacco
 	$tattiche[1]+=50;}
 	if($percsalute<10){//se la salute è pessima resa
-	$tattiche['2']+=101;}
+	$tattiche[2]+=101;}
 	if($percenergia2<5){//se esausto resa
-	$tattiche['2']+=150;}
+	$tattiche[2]+=150;}
 	$max=0;
 	foreach($tattiche as $chiave=>$elemento){
 	if($elemento>$max){
