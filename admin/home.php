1f12d4b9dcf13759831d5efdaaefa58b81dd4103
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-24 15:03:11 +0000

    fix

--- admin/home.php
+++ admin/home.php
@@ -10,10 +10,10 @@ $utenti=$db->QuerySelect("SELECT COUNT(*) AS id FROM utenti");
 echo "Utenti registrati: ".$utenti['id'].", ";
 $seonline=$adesso-600;
 $online=$db->QuerySelect("SELECT COUNT(*) AS id FROM utenti WHERE ultimazione>'".$seonline."'");
-echo "Utenti online: ".$online['id']", ";
+echo "Utenti online: ".$online['id'].", ";
 $seonline=$adesso-86400;
 $online=$db->QuerySelect("SELECT COUNT(*) AS id FROM utenti WHERE ultimazione>'".$seonline."'");
-echo "Utenti online ultime 24 ore: ".$online['id']"<br/><br/>";
+echo "Utenti online ultime 24 ore: ".$online['id']."<br/><br/>";
 }/* fine per ogni server*/ ?>
 <br/>
 <br/>
