39a3f6a13e6711385953d74d0b1123f3a78d42ae
Fabio Fantoni <fantonifabio@tiscali.it>
2008-06-24 11:13:26 +0000

    fix

--- game/inclusi/funzioni_combact.php
+++ game/inclusi/funzioni_combact.php
@@ -332,7 +332,7 @@ $dc->Stabilisciordine($battle['attid'],$battle['difid'],$battle);
 if($turni==0){
 $input.=$dc->Viewequip(1);
 $input.=$dc->Viewequip(2);}
-$input.=tattica(1,1)." - "tattica(2,1);//visualizzazione tattiche per debug
+$input.=$dc->tattica(1,1)." - "$dc->tattica(2,1);//visualizzazione tattiche per debug
 if($dc->tattica(1,1)!=2 AND $dc->tattica(2,1)!=2){
 $dc->Controllastato(1);
 $dc->Controllastato(2);
