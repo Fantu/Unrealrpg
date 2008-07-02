f21c3b6b771464851b63c8efe2b5ecc004189ca0
Fabio Fantoni <fantonifabio@tiscali.it>
2008-07-02 08:30:42 +0000

    miglioramento

--- game/archiviorep.php
+++ game/archiviorep.php
@@ -5,7 +5,7 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 }
 $serep=$db->QuerySelect("SELECT COUNT(id) AS num FROM battlereport WHERE attid='".$user['userid']."' OR difid='".$user['userid']."'");
 if ($serep['num']>0){//controllo se ci sono rep
-$reps=$db->QueryCiclo("SELECT * FROM battlereport WHERE attid='".$user['userid']."' OR difid='".$user['userid']."'");
+$reps=$db->QueryCiclo("SELECT * FROM battlereport WHERE attid='".$user['userid']."' OR difid='".$user['userid']."' ORDER BY id DESC");
 while($rep=$db->QueryCicloResult($reps)){
 if($rep['finito']==1)
 $link[]='<a href="index.php?loc=combact&do=repview&id='.$rep['id'].'">'.$lang['combact_avvenuto'].date($lang['dataora'],$rep['data']).'</a>';
