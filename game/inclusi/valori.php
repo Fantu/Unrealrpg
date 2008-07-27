c53778ec39fa8c4aedb579f21743a26691cb9b7b
Fabio Fantoni <fantonifabio@tiscali.it>
2008-07-27 11:57:55 +0000

    Rilasciata BETA 0.6.16

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -39,6 +39,10 @@ document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"IS
 "unieuro"=>"<script type=\"text/javascript\">
 var uri = 'http://imp.tradedoubler.com/imp?type(js)pool(276956)a(1316148)' + new String (Math.random()).substring (2, 11);
 document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"ISO-8859-1\"></sc'+'ript>');
+</script>",
+"ebay"=>"<script type=\"text/javascript\">
+var uri = 'http://imp.tradedoubler.com/imp?type(js)pool(280642)a(1316148)' + new String (Math.random()).substring (2, 11);
+document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"ISO-8859-1\"></sc'+'ript>');
 </script>"
 );
 ?>
