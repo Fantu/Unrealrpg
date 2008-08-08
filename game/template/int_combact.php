d8a9e61b78bf26b9ed0695ccb4ae9f3f7915ae04
Fabio Fantoni <fantonifabio@tiscali.it>
2008-08-08 14:08:54 +0000

    fix

--- game/template/int_combact.php
+++ game/template/int_combact.php
@@ -16,7 +16,7 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 <?php echo $outputcombact; ?>
 <br />
 <?php if($combactview==2){ ?>
-<?php echo "<a href=\"#fondo\">".$lang['seleziona_tattica']."</a>"; ?>
+<?php echo "<a name=\"fondo\">".$lang['seleziona_tattica']."</a>"; ?>
 <div align="center" id="tattic">
 <table border="0" align="center">
 <tr><td>
