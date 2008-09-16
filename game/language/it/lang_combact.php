d7ecf3361bfaf447f18c148f6adc05786cb42b3b
Fabio Fantoni <fantonifabio@tiscali.it>
2008-09-16 11:32:05 +0000

    miglioramento

--- game/language/it/lang_combact.php
+++ game/language/it/lang_combact.php
@@ -61,7 +61,7 @@ $lang['le_montagne']='Le Montagne';
 $lang['tempo_previsto_per_destinazione']='Per giungere a destinazione ci metterai circa %s minuti';
 $lang['oconfini_trovato_tesoro']='Hai trovato un piccolo tesoro dal valore di %s monete';
 $lang['oconfini_trovato_nulla']='Non hai trovato niente e nessuno';
-$lang['ocnord_desc']='Tempo stimato: %s minuti';
+$lang['ocnord_desc']='Tempo stimato di arrivo: %s minuti - Probabilità di incontrare nemici: media';
 
 $lang['nomepcpu1']='CpuProva';
 $lang['nomepcpu2']='Bandito giovane';
