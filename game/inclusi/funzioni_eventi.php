3f18a101942822106a20821bef6b2541ec89cb3b
Fabio Fantoni <fantonifabio@tiscali.it>
2008-09-18 17:57:58 +0000

    fix

--- game/inclusi/funzioni_eventi.php
+++ game/inclusi/funzioni_eventi.php
@@ -658,12 +658,12 @@ $energia=$secondi/100*3;
 $usercar=$db->QuerySelect("SELECT * FROM caratteristiche WHERE userid='".$userid."' LIMIT 1");	
 if($usercar['energia']<(50+$energia)){
 $energiam=($energia+50)-$usercar['energia'];
-$secondi+=100+($energia*60);
+$secondi2=100+($energia*60);
 $energia=50;
 }else{
 $energia=$usercar['energia']-$energia;
 }
-$db->QueryMod("UPDATE caratteristiche SET energia='".$energia."',recuperosalute='".$adesso."',recuperoenergia='".$adesso."' WHERE userid='".$userid."'");
-$db->QueryMod("INSERT INTO eventi (userid,datainizio,secondi,dettagli,tipo) VALUES ('".$userid."','".$adesso."','".$secondi."','16','9')");	
+$db->QueryMod("UPDATE caratteristiche SET energia='".$energia."',recuperosalute='".($adesso+$secondi)."',recuperoenergia='".($adesso+$secondi)."' WHERE userid='".$userid."'");
+$db->QueryMod("INSERT INTO eventi (userid,datainizio,secondi,dettagli,tipo) VALUES ('".$userid."','".$adesso."','".$secondi2."','16','9')");	
 } //fine Completaquest
 ?>
