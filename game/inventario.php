3605cc104ccf2014efc89df836ed21e0e1ccffa2
Fabio Fantoni <fantonifabio@tiscali.it>
2008-10-11 09:13:53 +0000

    fix

--- game/inventario.php
+++ game/inventario.php
@@ -3,7 +3,7 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	header("Location: ../index.php?error=16");
 	exit();
 }
-require('language/'.$language.'/lang_inventario.php');
+require_once('language/'.$language.'/lang_inventario.php');
 if (isset($_POST['vendi'])){
 $errore="";
 $quanti=(int)$_POST['quanti'];
@@ -76,7 +76,7 @@ $seoggetti=$db->QuerySelect("SELECT COUNT(*) AS id FROM inoggetti WHERE userid='
 if ($seoggetti['id']==0){
 $nessunogg=$lang['nessun_oggetto_posseduto'];
 }else{
-require('language/it/lang_oggetti_nomi.php');	
+require_once('language/'.$language.'/lang_oggetti_nomi.php');	
 $oggposseduti=$db->QueryCiclo("SELECT oggid,count(*) AS numero FROM inoggetti WHERE userid='".$user['userid']."' GROUP BY oggid");
 while($ogg=$db->QueryCicloResult($oggposseduti)) {
 $i++;
