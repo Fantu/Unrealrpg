4e4704b537f1d84cc94a283a3bb793b619d03be5
Fabio Fantoni <fantonifabio@tiscali.it>
2008-10-15 14:51:43 +0000

    miglioramenti

--- game/template/int_mostraoggetto.php
+++ game/template/int_mostraoggetto.php
@@ -9,9 +9,9 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 <?php if($oggetto['energia']>0){ echo $lang['energianec'].": ".$oggetto['energia']."<br />"; } ?>
 <?php if($oggetto['forzafisica']>0){ echo $lang['forzafisicanec'].": ".$oggetto['forzafisica']."<br />"; } ?>
 <?php if($oggetto['destrezza']>0){ echo $lang['destrezzanec'].": ".$oggetto['destrezza']."<br />"; } ?>
-<?php echo $lang['bonuseff'].": ".$oggetto['bonuseff']; ?>&#37;<br />
-<?php echo $lang['probtrovare'].": ".$oggetto['probtrovare']; ?><br />
-<?php echo $lang['probrottura'].": ".$oggetto['probrottura']; ?><br />
+<?php if($oggetto['bonuseff']>0){ echo $lang['bonuseff'].": ".$oggetto['bonuseff']."&#37;<br />"; } ?>
+<?php if($oggetto['probtrovare']>0){ echo $lang['probtrovare'].": ".$oggetto['probtrovare']."<br />"; } ?>
+<?php if($oggetto['probrottura']>0){ echo $lang['probrottura'].": ".$oggetto['probrottura']."<br />"; } ?>
 <?php if($oggetto['recsalute']>0){ echo $lang['recsalute'].": ".$oggetto['recsalute']."<br />"; } ?>
 <?php if($oggetto['recenergia']>0){ echo $lang['recenergia'].": ".$oggetto['recenergia']."<br />"; } ?>
 <?php if($oggetto['danno']>0){ echo $lang['danno'].": ".$oggetto['danno']."<br />"; } ?>
