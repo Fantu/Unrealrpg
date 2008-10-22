bf60311975e86f3a01851a2f0651659ee3584dea
Fabio Fantoni <fantonifabio@tiscali.it>
2008-10-22 17:24:42 +0000

    debug

--- game/inclusi/funzioni_combact.php
+++ game/inclusi/funzioni_combact.php
@@ -242,7 +242,8 @@ class Dati{
 	$this->che[$chi]->tattica=1;
 	}//se difesa prob di attacco
 	if($this->tattica($chi,1)==1){
-	if(($this->equip($chi,'cac')!=0) AND ($this->equip($chi,'adi')==0)){
+	$this->che[$chi]->tattica=3;
+	/*if(($this->equip($chi,'cac')!=0) AND ($this->equip($chi,'adi')==0)){
 	$this->che[$chi]->subtattica==1;
 	}elseif(($this->equip($chi,'cac')==0) AND ($this->equip($chi,'adi')!=0)){
 	$this->che[$chi]->subtattica==2;
@@ -253,7 +254,7 @@ class Dati{
 	$this->che[$chi]->subtattica==1;
 	else
 	$this->che[$chi]->subtattica==2;
-	}//se ha entrambe le armi
+	}//se ha entrambe le armi*/
 	}//se si attacca
 	}//fine Autotattic
 
