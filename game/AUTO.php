63769733a8de41d9d24a7a25723fd025b874dcef
Fabio Fantoni <fantonifabio@tiscali.it>
2008-10-25 12:25:11 +0000

    miglioramenti

--- game/AUTO.php
+++ game/AUTO.php
@@ -5,23 +5,26 @@ if($_GET['code']!='autourbg'){
 require('inclusi/valori.php');
 require_once('inclusi/funzioni.php');
 require('inclusi/funzioni_db.php');
-$db = new ConnessioniMySQL();
+$db=new ConnessioniMySQL();
 $adesso=strtotime("now");
 $int_security=$game_se_code;
+foreach($game_language as $chiavel=>$elementol){
+$language=$chiavel;
+require('language/'.$language.'/lang_interno.php');
+require('language/'.$language.'/lang_oggetti_nomi.php');
 foreach($game_server as $chiave=>$elemento){
+if($game_server_lang[$chiave]==$chiavel){
 $db->database=$chiave;
 $config=$db->QuerySelect("SELECT * FROM config");
 if($config['chiuso']==0){
-$language=$game_server_lang[$chiave];
-require('language/'.$language.'/lang_interno.php');
 require_once('inclusi/controllo_eventi.php');
 Controllaeventi(3);
 $semorti=$db->QuerySelect("SELECT COUNT(userid) AS id FROM caratteristiche WHERE saluteattuale<'1'");
-if ($semorti['id']>0){//se ci sono morti
+if($semorti['id']>0){//se ci sono morti
 $morti=$db->QueryCiclo("SELECT * FROM caratteristiche WHERE saluteattuale<'1'");
-while($morto=$db->QueryCicloResult($morti)) {
+while($morto=$db->QueryCicloResult($morti)){
 $eventi=$db->QuerySelect("SELECT COUNT(*) AS id FROM eventi WHERE userid='".$morto['userid']."'");
-if($eventi['id']==0) {
+if($eventi['id']==0){
 $user=$db->QuerySelect("SELECT * FROM utenti WHERE userid='".$morto['userid']."' LIMIT 1");
 $usercar=$db->QuerySelect("SELECT * FROM caratteristiche WHERE userid='".$morto['userid']."' LIMIT 1");
 require('inclusi/morte.php');
@@ -35,6 +38,8 @@ Controllacrimine($config);
 $tempopcrimine=3600-($config['utenti']*20);
 if($tempopcrimine<600){$tempopcrimine=600;}
 $db->QueryMod("UPDATE config SET atticriminali='".($adesso+$tempopcrimine)."'");}
-}//se il server non è chiuso
+}//fine se il server non è chiuso
+}//fine se è di quella lingua
 }//fine ogni server
+}//fine per ogni lingua
 ?>
