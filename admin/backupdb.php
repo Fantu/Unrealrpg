d3feae49c2be5588d223f3aacb38d1ccae96e727
Fabio Fantoni <fantonifabio@tiscali.it>
2008-10-26 21:10:05 +0000

    prova

--- admin/backupdb.php
+++ admin/backupdb.php
@@ -26,15 +26,13 @@ unlink($sqlfile);
 
 error_reporting(E_ALL);
 
-$_FILES=$HTTP_POST_FILES;
-
 $oggetto=$game_name." - ".$game_version." DB Backup ".date('d/m/Y');
 $mail="server@lostage.it";
 $contenuto="Backup db:".$dbsalvati;
 
 $tipofile=filetype($pallegato);
 $dimfile=filesize($pallegato);
-
+/*
 // DELIMITATORE
 $boundary=md5(uniqid(microtime()));
 
@@ -63,7 +61,31 @@ $mail_body.="Content-type: ".$tipofile."; name=\"".$allegato."\"\n";
 $mail_body.="Content-Transfer-Encoding: base64\n";
 $mail_body.="".$encoded_attach."\n";
 $mail_body.="--".$boundary."--\n";
+*/
 
+$mail_boundary = md5(uniqid(microtime()));
+
+$mail_headers="From: ".$mail."\n";
+$mail_headers.="MIME-Version: 1.0\r\n";
+$mail_headers.="Content-type: multipart/mixed; boundary=\"".$mail_boundary."\"";
+$mail_headers.="\r\n\r\n";
+$mail_headers.="This is a multi-part message in MIME format. ";
+$mail_headers.="\r\n\r\n";
+
+$file=fread(fopen($pallegato, "r"), $dimfile);
+$file=chunk_split(base64_encode($file));
+
+$mail_body="--".$mail_boundary."\n";
+$mail_body .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+$mail_body .= "Content-transfer-encoding:8 bit\r\n\r\n";
+$mail_body .= "".$contenuto."\n\n\n\n";
+$mail_body .= "--".$mail_boundary."\n";
+$filename=basename($allegato);
+$mail_body.="Content-type:application/octet-stream; name=".$filename."\r\n";
+$mail_body.="Content-transfer-encoding:base64\r\n\r\n";
+$mail_body.=$file."\r\n\r\n";
+$mail_body.="--".$mail_boundary."--\r\n";
+	
 // INVIO DELLA MAIL
 if(@mail("fantonifabio@tiscali.it", $oggetto, $mail_body, $mail_headers)) {// SE L'INVIO E' ANDATO A BUON FINE...
 echo "<p>La mail è stata inoltrata con successo.</p>";
