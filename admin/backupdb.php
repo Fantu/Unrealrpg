932363d17d8645113b370f028c17e0b6f41962a3
Fabio Fantoni <fantonifabio@tiscali.it>
2008-10-26 20:49:48 +0000

    fix

--- admin/backupdb.php
+++ admin/backupdb.php
@@ -33,7 +33,7 @@ $mail="server@lostage.it";
 $contenuto="Backup db:".$dbsalvati;
 
 $tipofile=filetype($pallegato);
-$dimfile=filesize($pallegato); 
+$dimfile=filesize($pallegato);
 
 // DELIMITATORE
 $boundary=md5(uniqid(microtime()));
@@ -42,33 +42,33 @@ $boundary=md5(uniqid(microtime()));
 $file=@fopen($pallegato, "r");
 $contents=@fread($file, $dimfile);
 $encoded_attach=chunk_split(base64_encode($contents));
-@fclose($file); 
+@fclose($file);
 
 // INTESTAZIONI DELLA MAIL
 $mail_headers="MIME-version: 1.0\n";
 $mail_headers.="Content-type: multipart/mixed; boundary=\"".$boundary."\"";
 $mail_headers.="X-attachments: ".$allegato."\n";
-$mail_headers.="From: ".$mail."\r\n"; 
+$mail_headers.="From: ".$mail."\r\n";
 
 
 // COSTRUIAMO IL CORPO DELLA MAIL
-$mail_body="Content-disposition: attachment; filename =\"$allegato\"\n\n";
-$mail_body.="--$boundary\n";
-$mail_body.="Content-Type: text/plain; charset=us-ascii\n";
+$mail_body="Content-disposition: attachment; filename =\"".$allegato."\"\n\n";
+$mail_body.="--".$boundary."--\n";
+$mail_body.="Content-Type: text/plain; charset=utf8\n";
 $mail_body.="Content-Transfer-Encoding: 7bit\n\n";
-$mail_body.="Contenuto: $contenuto\n\n";
-$mail_body.="Soggetto: $oggetto\n\n";
-$mail_body.="--$boundary\n";
-$mail_body.="Content-type: $tipofile; name=\"$allegato\"\n";
+$mail_body.="Contenuto: ".$contenuto."\n\n";
+$mail_body.="Soggetto: ".$oggetto."\n\n";
+$mail_body.="--".$boundary."\n";
+$mail_body.="Content-type: ".$tipofile."; name=\"".$allegato."\"\n";
 $mail_body.="Content-Transfer-Encoding: base64\n";
-$mail_body.="$encoded_attach\n";
-$mail_body.="--$boundary--\n";
+$mail_body.="".$encoded_attach."\n";
+$mail_body.="--".$boundary."--\n";
 
 // INVIO DELLA MAIL
 if(@mail("fantonifabio@tiscali.it", $oggetto, $mail_body, $mail_headers)) {// SE L'INVIO E' ANDATO A BUON FINE...
 echo "<p>La mail è stata inoltrata con successo.</p>";
 }else{// ALTRIMENTI...
-echo "<p>Si sono verificati dei problemi nell'invio della mail.</p>"; 
+echo "<p>Si sono verificati dei problemi nell'invio della mail.</p>";
 }
 
 unlink($pallegato);
