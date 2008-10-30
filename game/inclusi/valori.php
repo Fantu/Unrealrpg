c03cf29e3067f703d35af71a01c9bff5f487921b
Fabio Fantoni <fantonifabio@tiscali.it>
2008-10-30 09:28:48 +0000

    prova

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -1,7 +1,7 @@
 <?php
 $game_name="Unreal Rpg - Browser game";
 $game_version="DEV";
-$game_revision="0.7.11";
+$game_revision="0.7.12";
 $game_link="http://unrealff.it/rpgdev";
 $game_server=array(999=>"DEV",998=>"DEV");
 $game_language=array("it"=>"Italiano","en"=>"English");
