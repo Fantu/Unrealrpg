53e049f7912079c8e17be5849bda3537f6699f0e
Fabio Fantoni <fantonifabio@tiscali.it>
2008-10-30 22:21:17 +0000

    aggiunte

--- admin/inclusi/aggiornadb.php
+++ admin/inclusi/aggiornadb.php
@@ -12,43 +12,17 @@ if($check['version']!=$newversion AND $newversion==$game_revision){
 
 $db->QueryMod("UPDATE `pcpudata` SET `eqarm` = '86' WHERE `pcpudata`.`id` =6 LIMIT 1");
 $db->QueryMod("UPDATE `pcpudata` SET `eqarm` = '85',`eqscu` = '88' WHERE `pcpudata`.`id` =4 LIMIT 1");
-
-	/*//creazione record per tab con 1 record per utente
-	$a=$db->QueryCiclo("SELECT userid FROM utenti WHERE conferma='1' AND personaggio='1'");
-	while($var=$db->QueryCicloResult($a))
-	{
-		$db->QueryMod("INSERT INTO equipaggiamento (userid) VALUES ('".$var['userid']."')");
-	}*/
-
-/* INIZIO SISTEMAZIONE CARATTERISTICHE E RITORNO A LIVELLO 1
-require('../game/inclusi/personaggio.php');
-$a=$db->QueryCiclo("SELECT userid FROM utenti WHERE conferma='1' AND personaggio='1'");
-while($var=$db->QueryCicloResult($a))
-{
-$car=$db->QuerySelect("SELECT * FROM caratteristiche WHERE userid='".$var['userid']."'");
-if($car['livello']>1){
-$chiave2=$car['classe'];
-		$agilita=$classi['agilita'][$chiave2];
-		$attfisico=$classi['attfisico'][$chiave2];
-		$attmagico=$classi['attmagico'][$chiave2];
-		$diffisica=$classi['diffisica'][$chiave2];
-		$difmagica=$classi['difmagica'][$chiave2];
-		$mana=$classi['mana'][$chiave2];
-		$velocita=$classi['velocita'][$chiave2];
-		$intelligenza=$classi['intelligenza'][$chiave2];
-		$destrezza=$classi['destrezza'][$chiave2];
-$livello=$car['livello'];
-$exp=0;
-for($i=1;$i<=$livello-1;$i++){
-$expinc=1+floor($i/2);
-$exp+=$expinc*(120*$i);
-}
-$db->QueryMod("UPDATE caratteristiche SET agilita='".$agilita."',attfisico='".$attfisico."',attmagico='".$attmagico."',diffisica='".$diffisica."',difmagica='".$difmagica."',mana='".$mana."',manarimasto='".$mana."',velocita='".$velocita."',intelligenza='".$intelligenza."',destrezza='".$destrezza."',livello='1',exp=exp+'".$exp."',salute='100',saluteattuale='100',energia='1000',energiamax='1000' WHERE userid='".$var['userid']."'");
-}//se livello maggiore di 1
-}//per ogni utente
-// FINE SISTEMAZIONE CARATTERISTICHE E RITORNO A LIVELLO 1*/
-
-
+$db->QueryMod("CREATE TABLE `userlog` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`userid` SMALLINT UNSIGNED NOT NULL ,
+`testo` VARCHAR( 1000 ) NOT NULL ,
+`data` INT( 13 ) UNSIGNED NOT NULL
+) ENGINE = MYISAM");
+$db->QueryMod("CREATE TABLE `systemlog` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`testo` TEXT NOT NULL ,
+`data` INT( 13 ) UNSIGNED NOT NULL
+) ENGINE = MYISAM");
 
 $db->QueryMod("UPDATE `config` SET version='".$newversion."' WHERE id=".$chiave);
 echo sprintf($lang['aggiornato_db_server'],$chiave,$newversion)."<br />";
