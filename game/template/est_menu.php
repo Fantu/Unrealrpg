2a64f96f43b242883249b9b316ec5e3431b3802b
Fabio Fantoni <fantonifabio@tiscali.it>
2008-11-02 19:34:40 +0000

    fix

--- game/template/est_menu.php
+++ game/template/est_menu.php
@@ -3,7 +3,7 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	header("Location: ../../index.php?error=16");
 	exit();
 }?>
-<div id="menu2">
-<a href="index.php?pag=home"><?php echo "Home"; ?></a>
+<div id="menu">
+<a href="index_<?php echo $language; ?>.php?pag=home"><?php echo "Home"; ?></a>
 <a href="http://www.lostgames.net/forum/forumdisplay.php?f=33" target="_blank">Forum</a>
 </div>
