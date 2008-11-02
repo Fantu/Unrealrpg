56efbd1e3bdb71e2c61ec13582943f274d859b99
Fabio Fantoni <fantonifabio@tiscali.it>
2008-11-02 20:13:16 +0000

    miglioramento

--- game/template/est_footer.php
+++ game/template/est_footer.php
@@ -24,13 +24,13 @@ echo $lang['Informazioni_sui_server']; ?>
 <br /><br />
 <a href="http://validator.w3.org/check?uri=referer" target="_blank">
 	<img id="xhtml" src="game/template/immagini/xhtml_grigio.gif" alt="" border="0" onmouseover="CambiaImg('xhtml', true);" onmouseout="CambiaImg('xhtml', false);" />
-</a>&nbsp;&nbsp;
+</a>
 <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">
 	<img id="css" src="game/template/immagini/css_grigio.gif" border="0" alt="" onmouseover="CambiaImg('css', true);" onmouseout="CambiaImg('css', false);" />
 </a>
 <a href="http://www.php.net" target="_blank">
 	<img id="php" src="game/template/immagini/php_grigio.gif" alt="" border="0" onmouseover="CambiaImg('php', true);" onmouseout="CambiaImg('php', false);" />
-</a>&nbsp;&nbsp;
+</a>
 <a href="http://www.mysql.com" target="_blank">
 	<img id="mysql" src="game/template/immagini/mysql_grigio.gif" alt="" border="0" onmouseover="CambiaImg('mysql', true);" onmouseout="CambiaImg('mysql', false);" />
 </a><br /><br />
