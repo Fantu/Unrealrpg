6156ed0c861b4e569302d84e8536258ff5e8a11b
Fabio Fantoni <fantonifabio@tiscali.it>
2008-11-02 13:19:16 +0000

    prova

--- game/template/est_footer.php
+++ game/template/est_footer.php
@@ -1,8 +1,8 @@
 <?php
 if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	header("Location: ../../index.php?error=16");
-	exit(); ?>
-<?php echo $lang['Informazioni_sui_server']; ?>
+	exit();
+echo $lang['Informazioni_sui_server']; ?>
 <table width="750" border="0" align="center">
 <tr>
 <td><?php echo $lang['Nome']."</td><td>".$lang['Utenti_registrati']."</td><td>".$lang['Ultima_settimana']."</td><td>".$lang['Utenti_online']."</td><td>".$lang['Ultimo_giorno']; ?></td>
