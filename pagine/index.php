46663866ef203e9b253b4a0aad4d25e34d5365f4
Fabio Fantoni <fantonifabio@tiscali.it>
2008-11-04 08:58:57 +0000

    prova

--- pagine/index.php
+++ pagine/index.php
@@ -18,7 +18,7 @@ $pagina=htmlspecialchars($_GET['pag'],ENT_QUOTES);
 if(!file_exists('pagine/'.$pagina.'.php') OR $pagina=="index")
 $pagina="home";
 require('game/template/est_header.php');
-echo "<div style=\"height:150px;overflow:auto;\"><table width=\"750\" border=\"0\" align=\"center\"><tr><td width=\"160\" valign=\"top\">";
+echo "<div style=\"height:200px;\"><table width=\"750\" border=\"0\" align=\"center\"><tr><td width=\"160\" valign=\"top\">";
 require('game/template/est_menu.php');
 echo "</td><td width=\"590\">";
 require('pagine/'.$pagina.'.php');
