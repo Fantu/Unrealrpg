89ce2e922386e70f2d363882580b9dd33dd758d7
Fabio Fantoni <fantonifabio@tiscali.it>
2008-11-30 11:01:29 +0000

    fix

--- admin/admin.php
+++ admin/admin.php
@@ -1,6 +1,7 @@
 <?php
 require('../game/inclusi/valori.php');
 require('../game/inclusi/funzioni_db.php');
+require('../game/inclusi/funzioni.php');
 $db=new ConnessioniMySQL();
 $db->database=1000;
 $int_security=$game_se_code;
