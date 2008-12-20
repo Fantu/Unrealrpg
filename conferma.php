2896fcd77f94aadf50f4c77a455732f74607f44b
Fabio Fantoni <fantonifabio@tiscali.it>
2008-12-20 16:35:13 +0000

    miglioramenti

--- conferma.php
+++ conferma.php
@@ -1,37 +1,26 @@
 <?php
 require('game/inclusi/valori.php');
 require('game/inclusi/funzioni_db.php');
-$db = new ConnessioniMySQL();
+$db=new ConnessioniMySQL();
 $indb=(int)$_GET['t'];
-$esistenza=0;		
-	foreach($game_server as $chiave=>$elemento){
-	if ($chiave==$indb){$esistenza=1;}
-	}
-if($esistenza==1)
-	$db->database=$indb;
-else {
-	header("Location: index.php?error=5");
-	exit();
-}
-if(!$_GET['cod']) {
+if(isset($game_server[$server])){$db->database=$indb;}else{header("Location: index.php?error=5"); exit();}
+if(!$_GET['cod']){
 	header("Location: index.php?error=5");
 	exit();
 }
 $code=htmlspecialchars($_GET['cod'],ENT_QUOTES);
 $a=$db->QuerySelect("SELECT userid,conferma FROM utenti WHERE codice='".$code."' LIMIT 1");
 
-if(!$a['userid']) {
+if(!$a['userid']){
 	header("Location: index.php?error=5");
 	exit();
 }
-else if($a['conferma']==1) {
+else if($a['conferma']==1){
 	header("Location: index.php?error=6");
 	exit();
-} else {	
-	$ora=strtotime("now");
+}else{
 	$db->QueryMod("UPDATE utenti SET conferma='1' WHERE userid='".$a['userid']."'");
-	$db->QueryMod("UPDATE config SET utenti=utenti+'1'");						
-	
+	$db->QueryMod("UPDATE config SET utenti=utenti+'1'");
 	header("Location: index.php?error=7");
 	exit();
 }
