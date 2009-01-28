25cd9b94da7d576a90502b29fcecee424e1dfe07
Fabio Fantoni <fantonifabio@tiscali.it>
2009-01-28 07:57:46 +0000

    miglioramento

--- admin/inclusi/aggiornadb.php
+++ admin/inclusi/aggiornadb.php
@@ -3,23 +3,14 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	header("Location: ../../index.php?error=16");
 	exit();
 }
-$newversion="0.7.16";
+$newversion="0.7.17";
 foreach($game_server as $chiave=>$elemento){
 if($chiave!=999){
 $db->Setdb($chiave);
 $check=$db->QuerySelect("SELECT version FROM config WHERE id=".$chiave);
 if($check['version']!=$newversion AND $newversion==$game_revision){
 
-$db->QueryMod("CREATE TABLE `sessione` (
-`id` VARCHAR( 32 ) NOT NULL ,
-`userid` SMALLINT UNSIGNED NOT NULL ,
-`password` VARCHAR( 32 ) NOT NULL ,
-`ip` VARCHAR( 15 ) NOT NULL ,
-`time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-PRIMARY KEY ( `id` )
-) ENGINE = MYISAM");
-$db->QueryMod("ALTER TABLE `sessione` CHANGE `time` `time` INT UNSIGNED NOT NULL");
-$db->QueryMod("ALTER TABLE `utenti` DROP `ipattuale`");
+$db->QueryMod("UPDATE `pcpudata` SET `salute` = '105',`energia` = '1050',`eqpoz` = '0' WHERE `pcpudata`.`id` =8 LIMIT 1");
 
 $db->QueryMod("UPDATE `config` SET version='".$newversion."' WHERE id=".$chiave);
 echo sprintf($lang['aggiornato_db_server'],$chiave,$newversion)."<br />";
