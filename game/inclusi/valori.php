7a0d53ded76f8df09969c45fcbfd6c3916a33616
Fabio Fantoni <fantonifabio@tiscali.it>
2009-06-22 14:54:58 +0000

    eliminati dai banner i circuiti esterni

--- game/inclusi/valori.php
+++ game/inclusi/valori.php
@@ -14,30 +14,6 @@ $adesso=strtotime("now");
 
 //banner 120*600
 $banner1=array(
-"default"=>"<script type=\"text/javascript\">
-var uri = 'http://imp.tradedoubler.com/imp?type(js)pool(275035)a(1316148)' + new String (Math.random()).substring (2, 11);
-document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"ISO-8859-1\"></sc'+'ript>');
-</script>",
-"nike"=>"<script type=\"text/javascript\">
-var uri = 'http://imp.tradedoubler.com/imp?type(js)pool(275541)a(1316148)' + new String (Math.random()).substring (2, 11);
-document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"ISO-8859-1\"></sc'+'ript>');
-</script>",
-"apple"=>"<script type=\"text/javascript\">
-var uri = 'http://imp.tradedoubler.com/imp?type(js)pool(275579)a(1316148)' + new String (Math.random()).substring (2, 11);
-document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"ISO-8859-1\"></sc'+'ript>');
-</script>",
-"multiplayer"=>"<script type=\"text/javascript\">
-var uri = 'http://imp.tradedoubler.com/imp?type(js)pool(276343)a(1316148)' + new String (Math.random()).substring (2, 11);
-document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"ISO-8859-1\"></sc'+'ript>');
-</script>",
-"unieuro"=>"<script type=\"text/javascript\">
-var uri = 'http://imp.tradedoubler.com/imp?type(js)pool(276956)a(1316148)' + new String (Math.random()).substring (2, 11);
-document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"ISO-8859-1\"></sc'+'ript>');
-</script>",
-"ebay"=>"<script type=\"text/javascript\">
-var uri = 'http://imp.tradedoubler.com/imp?type(js)pool(280642)a(1316148)' + new String (Math.random()).substring (2, 11);
-document.write('<sc'+'ript type=\"text/javascript\" src=\"'+uri+'\" charset=\"ISO-8859-1\"></sc'+'ript>');
-</script>",
 "facilesoft"=>"<a href=\"http://www.facilesoft.it/\" target=\"_blank\"><img src=\"http://lostage.it/game/file/facilesoft_120_600.jpg\" border=\"0\" alt=\"\" /></a>"
 );
 ?>
