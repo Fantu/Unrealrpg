62df920ce2f599cc1676d8a1893d242bca124f9c
Fabio Fantoni <fantonifabio@tiscali.it>
2009-07-18 16:30:53 +0000

    aggiornati i file di testo in inglese

---
+++ game/language/en/lang_gamelog.php
@@ -0,0 +1,13 @@
+<?php
+//inizio menu
+$lang['msglog1']="Login effettuato con ip {0}";//0=ip
+$lang['msglog2']="Depositate in banca {0} monete";//0=monete
+$lang['msglog3']="Prelevate dalla banca {0} monete";//0=monete
+$lang['msglog4']="Prese in prestito dalla banca {0} monete";//0=monete
+$lang['msglog5']="Restituite alla banca {0} monete di prestito";//0=monete
+$lang['msglog6']="Comprato biglietto della lotteria";
+$lang['msglog7']="Vinte {0} monete alla lotteria";//0=monete
+$lang['msglog8']="Iniziata preghiera nel tempio per {0} ore";//0=ore
+$lang['msglog9']="Pagate {0} monete ai chierici per la resurrezione";//0=monete
+$lang['msglog10']="Comprati {0} di {1} per un totale di {2} monete";//0=quantità 1=oggetto 2=totale monete pagate
+?>
