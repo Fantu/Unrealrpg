e8227f91efdb846eb8e279b05d7e6f0e038d9ae5
Fabio Fantoni <fantonifabio@tiscali.it>
2014-02-08 15:20:32 +0100

    Removed forum link, no more active.

--- game/template/est_menu.php
+++ game/template/est_menu.php
@@ -5,5 +5,4 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 }?>
 <div id="estmenu">
 <a href="index_<?php echo $language; ?>.php?pag=home"><?php echo "Home"; ?></a>
-<a href="http://www.lostgames.net/forum/forumdisplay.php?f=33" target="_blank">Forum</a>
 </div>
