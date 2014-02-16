e3f23e8194a069183fab6a97551826b525383156
Fabio Fantoni <fantonifabio@tiscali.it>
2014-02-16 20:16:16 +0100

    Improve the game database update to new version
    - add old version check for not do incompatible update
    - converted to utf8 and added indentation

--- admin/inclusi/aggiornadb.php
+++ admin/inclusi/aggiornadb.php
@@ -3,20 +3,19 @@ if((empty($int_security)) OR ($int_security!=$game_se_code)){
 	header("Location: ../../index.php?error=16");
 	exit();
 }
+$oldversion="0.8.0";
 $newversion="0.8.1";
 foreach($game_server as $chiave=>$elemento){
-if($chiave!=999){
-$db->Setdb($chiave);
-$check=$db->QuerySelect("SELECT version FROM config WHERE id=".$chiave);
-if($check['version']!=$newversion AND $newversion==$game_version){
+    $db->Setdb($chiave);
+    $check=$db->QuerySelect("SELECT version FROM config WHERE id=".$chiave);
+    if($check['version']==$oldversion AND $newversion==$game_version){
 
-$db->QueryMod("");
+    $db->QueryMod("");
 
-$db->QueryMod("UPDATE `config` SET version='".$newversion."' WHERE id=".$chiave);
-echo sprintf($lang['aggiornato_db_server'],$chiave,$newversion)."<br />";
-}/*se non aggiornato*/else{
-echo sprintf($lang['non_aggiornato_db_server'],$chiave)."<br />";
-}
-}//se non è quello di sviluppo principale
-}//fine per ogni server
+    $db->QueryMod("UPDATE `config` SET version='".$newversion."' WHERE id=".$chiave);
+    echo sprintf($lang['aggiornato_db_server'],$chiave,$newversion)."<br />";
+    }else{
+        echo sprintf($lang['non_aggiornato_db_server'],$chiave)."<br />";
+    }
+    }//end for each server
 ?>
