3dc8d652d51dce70455125787ab1bbe7b4aa417e
Fabio Fantoni <fantonifabio@tiscali.it>
2014-02-19 20:48:14 +0100

    Improved db backup script
    Fixed the bad header problem and other small improvements

--- admin/backupdb.php
+++ admin/backupdb.php
@@ -7,10 +7,10 @@ if($_GET['code']!=$scripts_se_code){header("Location: ../index.php?error=16"); e
 $path=MAIN_PATH."/admin/cache/";
 
 foreach($game_server as $chiave=>$elemento){
-$db->Setdb($chiave);
-$sqlfile=$path.$chiave."_".date('Y_m_d').".sql";
-$db->Dbdump($sqlfile);
-$dbsalvati.=" ".$chiave;
+    $db->Setdb($chiave);
+    $sqlfile=$path.$chiave."_".date('Y_m_d').".sql";
+    $db->Dbdump($sqlfile);
+    $dbsalvati.=" ".$chiave;
 }//per ogni regno
 
 
@@ -21,8 +21,8 @@ $tar="tar cvzf ".$pallegato." ".$path."*";
 exec($tar);
 
 foreach($game_server as $chiave=>$elemento){
-$sqlfile=$path.$chiave."_".date('Y_m_d').".sql";
-unlink($sqlfile);
+    $sqlfile=$path.$chiave."_".date('Y_m_d').".sql";
+    unlink($sqlfile);
 }//per ogni regno
 
 error_reporting(E_ALL);
@@ -32,35 +32,36 @@ $contenuto="Backup db:".$dbsalvati;
 
 $tipofile=filetype($pallegato);
 $dimfile=filesize($pallegato);
+$eol="\n";
 
 $mail_boundary = md5(uniqid(microtime()));
 
-$mail_headers="From: ".$game_mail."\n";
-$mail_headers.="MIME-Version: 1.0\r\n";
-$mail_headers.="Content-type: multipart/mixed; boundary=\"".$mail_boundary."\"";
-$mail_headers.="\r\n\r\n";
-$mail_headers.="This is a multi-part message in MIME format. ";
-$mail_headers.="\r\n\r\n";
+$mail_headers="From: ".$game_mail.$eol;
+$mail_headers.="MIME-Version: 1.0".$eol;
+$mail_headers.="Content-type: multipart/mixed; boundary=\"".$mail_boundary."\"".$eol.$eol;
+$mail_headers.="Content-Transfer-Encoding: 7bit".$eol;
+$mail_headers.="This is a multi-part message in MIME format. ".$eol.$eol;
 
 $file=fread(fopen($pallegato, "r"), $dimfile);
 $file=chunk_split(base64_encode($file));
 
-$mail_body="--".$mail_boundary."\n";
-$mail_body.="Content-type:text/plain; charset=utf8\r\n";
-$mail_body.="Content-transfer-encoding:8 bit\r\n\r\n";
-$mail_body.="".$contenuto."\n\n\n\n";
-$mail_body.="--".$mail_boundary."\n";
+$mail_body="--".$mail_boundary.$eol;
+$mail_body.="Content-type:text/plain; charset=utf8".$eol;
+$mail_body.="Content-transfer-encoding:8 bit".$eol.$eol;
+$mail_body.=$contenuto.$eol.$eol;
+$mail_body.="--".$mail_boundary.$eol;
 $filename=basename($allegato);
-$mail_body.="Content-type:application/octet-stream; name=".$filename."\r\n";
-$mail_body.="Content-transfer-encoding:base64\r\n\r\n";
-$mail_body.=$file."\r\n\r\n";
-$mail_body.="--".$mail_boundary."--\r\n";
+$mail_body.="Content-type:application/octet-stream; name=".$filename.$eol;
+$mail_body.="Content-transfer-encoding:base64".$eol.$eol;
+$mail_body.="Content-Disposition: attachment".$eol.$eol;
+$mail_body.=$file.$eol.$eol;
+$mail_body.="--".$mail_boundary."--".$eol;
 
 // INVIO DELLA MAIL
 if(@mail($game_mail,$oggetto,$mail_body,$mail_headers)){// SE L'INVIO E' ANDATO A BUON FINE...
-echo "<p>La mail è stata inoltrata con successo.</p>";
+    echo "<p>La mail è stata inoltrata con successo.</p>";
 }else{// ALTRIMENTI...
-echo "<p>Si sono verificati dei problemi nell'invio della mail.</p>";
+    echo "<p>Si sono verificati dei problemi nell'invio della mail.</p>";
 }
 
 unlink($pallegato);
